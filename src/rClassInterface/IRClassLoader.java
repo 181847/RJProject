0dd441550546c9e7f0294467304bec9502dfaea9
181847 <753090084@qq.com>
2017-01-22 19:07:45 +0800

    小细节更改
    一个无关紧要的注释修改

--- src/rClassInterface/IRClassLoader.java
+++ src/rClassInterface/IRClassLoader.java
@@ -1,5 +1,9 @@
 package rClassInterface;
-
+/*
+ * 本接口暂时被废弃，因为在程序中
+ * 只需要有一个RClassLoader，因此将这个类的所有防范设为Static
+ * 直接通过类名访问，不需要在另外划分空间设置引用保存一个RClassLoader
+ */
 
 public interface IRClassLoader{
 	//从指定的文件路径加载RClass，用一个int数值返回加载的结果
@@ -7,7 +11,7 @@ public interface IRClassLoader{
 	//检查两个RClass的相对关系
 	//case 1:同一类型，基本数据类型
 	//case 2:同一类型，普通类型，包括原子非基本数据类型
-	//case 3:不同类型，source为子类，如果到此，必定不是Java包装类，也不是基本数据类型
+	//case 3:不同类型，rClassB为子类，如果到此，必定不是Java包装类，也不是基本数据类型
 	int checkRClassMatchType(String rClassA, String rClassB);
 	//通过名字得到一个RClass
 	IRClass getRClass(String rClassName);
