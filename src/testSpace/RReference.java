4570ad1fd6d5b01a453322a5243ebf48b5140bbf
181847 <753090084@qq.com>
2017-01-22 20:42:44 +0800

    RReference成员修改和部分方法的实现
    RReference插入IRReference接口（注：IRReference继承与于INameable接口），继承NameableWithString（实现一个INameable接口）类。
    去除boolean
    isBasicType:成员，判断一个引用是否是基本数据类型通过将这个引用的名字放进RClassLoader.getTypeOf(referenceClass)来判断。
    
    RReference的构造函数只需要传入isAtom\referenceClass，接下来的工作交给init()函数来完成，当引用为基本数据类型时，需要对数据域赋予基本数据类型的实例对象，其他类型只需要将datas=null。
    定义引用的赋值方法boolean set();，内部的duplicateBasicData()有待进一步定义

--- src/testSpace/RReference.java
+++ src/testSpace/RReference.java
@@ -1,12 +1,9 @@
 package testSpace;
-import basicInterface.INameable;
 import rClassInterface.IRReference;
 
-public class RReference implements IRReference {
+public class RReference extends NameableWithString implements IRReference {
 	//是否是Java包装类型的引用
 	public boolean isAtom;
-	//是否是基本数据类型
-	public boolean isBasicType;
 	//数据域
 	public Object[] datas;
 	//成员数量
@@ -16,11 +13,99 @@ public class RReference implements IRReference {
 	//数据所属的RClass名称
 	public String dataClass;
 	
-
+	//构造方法，传入部分参数进行设置，然后调用init()方法设置memberNum\dataClass\datas
+	public RReference(boolean isAtom, String referenceClass){
+		this.isAtom = isAtom;
+		this.referenceClass = referenceClass;
+		init();
+	}
+	
+	//初始化方法，根据isAtom\referenceClass
+	//对这个RReference实例进行初始化设置
+	public void init(){
+		if (RClassLoader.getTypeOf(referenceClass)==0){
+			dataClass = referenceClass;
+			datas = new Object[1];
+			memberNum = 1;
+			try{
+				switch(referenceClass.charAt(0))
+				{
+				case 'B': 
+					switch(referenceClass.charAt(1)){
+					case 'y':
+						//对应Byte类型
+						datas[0] = new Byte("0");
+						break;
+						
+					case 'o':
+						//对应Boolean类型
+						datas[0] = new Boolean(false);
+						break;
+					}
+					break;
+					
+				case 'S':
+					//对应Short类型
+					datas[0] = new Short((short)0);
+					break;
+					
+				case 'I':
+					//对应Integer类型
+					datas[0] = new Integer(0);
+					break;
+				
+				case 'L':
+					//对应Long类型
+					datas[0] = new Long(0);
+					break;
+					
+				case 'F':
+					//对应Float类型
+					datas[0] = new Float(0);
+					break;
+					
+				case 'D':
+					//对应Double类型
+					datas[0] = new Double(0);
+					break;
+					
+				case 'C':
+					//对应Character类型
+					datas[0] = new Character('\0');
+					break;
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else{
+			datas = null;
+			dataClass = "";
+		}
+			
+	}
+	
+	
+	//用另一个RReference来对本RReference进行设置
+	//基本数据类型按值设置
+	//其他类型按引用设置
+	//现阶段只考虑相同类型的基本数据类型之间进行设置
 	@Override
 	public boolean set(IRReference source) {
-		// TODO Auto-generated method stub
-		return false;
+		switch(RClassLoader.checkRClassMatchType(referenceClass, source.getDataClass()))
+		{
+		case 1://同一类型，基本数据类型
+			duplicateBasicData(source);
+		case 2://同一类型，普通类型，包括原子非基本数据类型
+			datas = source.getObjects();
+		case 3:
+			//不同类型，source为子类，
+			//如果到此，必定不是Java包装类，也不是基本数据类型
+			datas = source.getObjects();
+			dataClass = source.getDataClass();
+		default:
+			return false;
+		}
 	}
 
 	@Override
@@ -31,7 +116,16 @@ public class RReference implements IRReference {
 
 	@Override
 	public Object[] getObjects() {
-		// TODO Auto-generated method stub
-		return null;
+		return datas;
+	}
+
+	@Override
+	public String getReferenceClass() {
+		return referenceClass;
+	}
+
+	@Override
+	public String getDataClass() {
+		return dataClass;
 	}
 }
