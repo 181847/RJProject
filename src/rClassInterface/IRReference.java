495fed7a631a9888ed3a22b8fd55007b72ebe018
181847 <753090084@qq.com>
2017-01-22 19:12:15 +0800

    IRReference接口修改
    到头来这些需要名字的接口还是要先继承INameable接口，使得这些接口具有与名字相关的方法，我们可以在后面具体实现这个接口时直接继承一个实现了INameable的类，从而就有接口的方法，又省去了每次实现INameable接口时都要定义一个字符串成员的麻烦，现在只是将IRReference的接口恢复了对INameable接口的继承，还有其他需要命名的接口等待修改

--- src/rClassInterface/IRReference.java
+++ src/rClassInterface/IRReference.java
@@ -1,12 +1,17 @@
 package rClassInterface;
+import basicInterface.INameable;
 
 
 
-public interface IRReference{
+public interface IRReference extends INameable{
 	//将source引用的对象赋给调用者
 	public boolean set(IRReference source);
 	//调用者和source确认是基本数据类型，基本数据类型数值复制
 	public void duplicateBasicData(IRReference source);
 	//返回引用的datas数组
 	public Object[] getObjects();
+	//返回引用的引用类型
+	public String getReferenceClass();
+	//返回引用的具体的数据的类型
+	public String getDataClass();
 }
