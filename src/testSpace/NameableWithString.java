16990916cf211a43f1d60659726a81fd642a9076
181847 <753090084@qq.com>
2017-01-22 16:08:24 +0800

    添加一个类实现INameable

---
+++ src/testSpace/NameableWithString.java
@@ -0,0 +1,14 @@
+package testSpace;
+import basicInterface.INameable;
+
+public class NameableWithString implements INameable
+{
+	String name;
+
+	@Override
+	public String getName()
+	{
+		// TODO: Implement this method
+		return name;
+	}
+}
