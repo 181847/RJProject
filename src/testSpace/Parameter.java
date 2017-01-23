a277e3b275a48e5332ec1abe98bbc505503c7720
181847 <753090084@qq.com>
2017-01-23 21:45:05 +0800

    实现IParameter接口的类，继承于RReference

---
+++ src/testSpace/Parameter.java
@@ -0,0 +1,33 @@
+package testSpace;
+import functionInterface.*;
+
+public class Parameter extends RReference implements IParameter{
+	IReturnval returnvalSource;
+	public Parameter(String parameterName, boolean isAtom, String referenceClass){
+		super(parameterName, isAtom, referenceClass);
+		returnvalSource = null;
+	}
+
+	@Override
+	public void extractParameter()
+	{
+		set(returnvalSource);
+	}
+
+	@Override
+	public void linkReturnval(IReturnval returnval)
+	{
+		returnvalSource = returnval;
+	}
+
+	@Override
+	public IReturnval getReturnval()
+	{
+		return returnvalSource;
+	}
+
+
+	
+	
+
+}
