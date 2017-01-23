58e05e38204eaeda4f28212d4fe0cea20c6e54fe
181847 <753090084@qq.com>
2017-01-23 18:24:45 +0800

    修改Integer类型RReference的测试用例，添加对Excutee与Excuter链接的测试用例

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -1,20 +1,29 @@
 package testSpace;
 import rClassInterface.*;
+import functionInterface.*;
 
 public class testClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IRReference ref;
-		ref = new RReference(true, "Integer");
-		
-		ref.setName("firstInt");
+		ref = new RReference("firstInt", true, "Integer");
 		
 		Object[] a = ref.getObjects();
 		
 		a[0] = new Integer(78);
 		
-		System.out.println(((Integer)(ref.getObjects()[0])).toString() );
+		System.out.println(ref.getName() +" is type of " + ref.getReferenceClass() + " whose value is " + ((Integer)(ref.getObjects()[0])).toString() );
+		
+		IExcutee excutee;
+		IExcuter excuter;
+		
+		excutee = new NormalExcutee("firstExcutee", null, 0);
+		excuter = new Excuter("firstExcuter");
+		
+		excuter.linkExcutee(excutee);
+		
+		System.out.println(excuter.getName() +" LINK " +excuter.getExcutee().getName());
 	}
 
 }
