7864116e1339899218b6a6dff3f1b18356d8108c
181847 <753090084@qq.com>
2017-01-23 21:52:44 +0800

    添加对于IParameter和IReturnval相链接的测试用例，修改先前对RReference的测试，改为对Parameter的RReference特性测试

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -7,7 +7,7 @@ public class testClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IRReference ref;
-		ref = new RReference("firstInt", true, "Integer");
+		ref = new Parameter("firstInt", true, "Integer");
 		
 		Object[] a = ref.getObjects();
 		
@@ -24,6 +24,16 @@ public class testClass {
 		excuter.linkExcutee(excutee);
 		
 		System.out.println(excuter.getName() +" LINK " +excuter.getExcutee().getName());
+		
+		IParameter parameter;
+		IReturnval returnval;
+		
+		parameter = new Parameter("firstParameter", true, "Integer");
+		returnval = new Returnval("firstReturnval", true, "Integer");
+		
+		parameter.linkReturnval(returnval);
+		
+		System.out.println("Parameter: " + parameter.getName() + " LINK Returnval: " +parameter.getReturnval().getName());
 	}
 
 }
