ce6fd2cf2576616f9c7926da30ece58e2db69a2a
181847 <753090084@qq.com>
2017-01-23 21:41:06 +0800

    IParameter（参数接口）继承IRReference（引用接口），增加链接Returnval的方法和返回Returnval的方法，extractParameter()方法添加public修饰符

--- src/functionInterface/IParameter.java
+++ src/functionInterface/IParameter.java
@@ -1,6 +1,11 @@
 package functionInterface;
+import rClassInterface.IRReference;
 
-public interface IParameter{
+public interface IParameter extends IRReference{
 	//从数据来源处获得数据，存储到一个RReference中
-	void extractParameter();
+	public void extractParameter();
+	//链接一个Returnval
+	public void linkReturnval(IReturnval returnval);
+	//返回链接的Returnval
+	public IReturnval getReturnval();
 }
