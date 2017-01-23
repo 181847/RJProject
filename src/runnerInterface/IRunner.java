02dd66b8f9639f7741c7d1cf68a923858217e802
181847 <753090084@qq.com>
2017-01-23 10:10:23 +0800

    IRunner添加两个方法用来设置Excutee的可执行状态以及Runner的遍历参数状态

--- src/runnerInterface/IRunner.java
+++ src/runnerInterface/IRunner.java
@@ -1,5 +1,11 @@
 package runnerInterface;
 
 public interface IRunner{
+	//Runner开始执行运行栈内的Excutee
 	public void run();
+	//设置在Runner当前的运行循环周期内
+	//是否执行当前Excutee的运算功能
+	public void setExcutedableState(boolean isExcutedable);
+	//设置当前Runner是否处于反向遍历参数的状态
+	public void setRetraverseParameterState(boolean isRetraverseParameter);
 }
