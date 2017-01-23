c484947a8ea1e5bd035c6fd34d2e5773acb97669
181847 <753090084@qq.com>
2017-01-23 17:21:34 +0800

    删除方法getNextExcutee()，添加public修饰符

--- src/functionInterface/IExcutee.java
+++ src/functionInterface/IExcutee.java
@@ -3,11 +3,9 @@ import runnerInterface.IRunner;
 
 public interface IExcutee{
 	//欢迎Runner的到来，检查参数是否遍历
-	void welcomeRunner(IRunner runner);
+	public void welcomeRunner(IRunner runner);
 	//发动hostFunction的运算功能
-	void fire();
+	public void fire();
 	//为Runner指明下一个要运行的地方
-	void sendRunner(IRunner runner);
-	//得到下一个将要发动的Excutee
-	IExcutee getNextExcutee();
+	public void sendRunner(IRunner runner);
 }
