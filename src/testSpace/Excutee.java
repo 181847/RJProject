fb3943605e722f4df1771e819572dc0a43b2d0d8
181847 <753090084@qq.com>
2017-01-23 12:01:49 +0800

    Excutee部分方法的实现：WelcomeRunner()完成，fire()方法未完成

--- src/testSpace/Excutee.java
+++ src/testSpace/Excutee.java
@@ -1,5 +1,6 @@
 package testSpace;
 import functionInterface.*;
+import runnerInterface.*;
 
 public class Excutee implements IExcutee
 {
@@ -7,4 +8,47 @@ public class Excutee implements IExcutee
 	public IFunction hostFunction;
 	public int paragraphToFire;
 	public int nextExcuterInRearSlot;
+
+	@Override
+	public void welcomeRunner(IRunner runner)
+	{
+		if (isReady){
+			//参数准备完成可运行
+			runner.setExcutedableState(true);
+			//关闭遍历参数状态
+			runner.setRetraverseParameterState(false);
+		}else{
+			//参数未遍历，不可执行
+			runner.setExcutedableState(true);
+			//打开参数遍历状态
+			runner.setRetraverseParameterState(false);
+			//向运行栈压入与参数链接的
+			//BaseCalculator的Excutee
+			runner.retraverseParameters(hostFunction.getParameterList());
+		}
+	}
+
+	@Override
+	public void fire()
+	{
+		hostFunction.run(paragraphToFire);
+	}
+
+	@Override
+	public void sendRunner(IRunner runner)
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public IExcutee getNextExcutee()
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+
+
+
+	
 }
