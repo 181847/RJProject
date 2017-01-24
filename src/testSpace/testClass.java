0f7abe81e3a1f354fada685ca868b0c8b548a607
181847 <753090084@qq.com>
2017-01-24 22:23:19 +0800

    修改先前测试用例的小错误，用接口引用来存储测试对象；增加对FunctionHeadSlot和FunctionRearSlot的测试用例

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -35,29 +35,47 @@ public class testClass {
 		
 		System.out.println("Parameter: " + parameter.getName() + " LINK Returnval: " +parameter.getReturnval().getName());
 		
-		ExcuteeList excuteeList = new ExcuteeList(3);
+		IExcuteeList excuteeList = new ExcuteeList(3);
 		
 		System.out.println(excuteeList.insertExcutee(excutee));
 		System.out.println(excuteeList.insertExcutee(new NormalExcutee("secondExcutee", null, 0)));
 		System.out.println(excuteeList.getExcutee("firstExcutee").getName());
 		
-		ExcuterList excuterList = new ExcuterList(3);
+		IExcuterList excuterList = new ExcuterList(3);
 		
 		System.out.println(excuterList.insertExcuter(excuter));
 		System.out.println(excuterList.insertExcuter(new Excuter("secondExcuter")));
 		System.out.println(excuterList.getExcuter("firstExcuter").getExcutee().getName());
 		
-		ParameterList parameterList = new ParameterList(3);
+		IParameterList parameterList = new ParameterList(3);
 		
 		System.out.println(parameterList.insertParameter(parameter));
 		System.out.println(parameterList.insertParameter(new Parameter("secondParameter", true, "Integer")));
 		System.out.println(parameterList.getParameter("firstParameter").getReturnval().getName());
 		
-		ReturnvalList returnvalList = new ReturnvalList(3);
+		IReturnvalList returnvalList = new ReturnvalList(3);
 		
 		System.out.println(returnvalList.insertReturnval(returnval));
 		System.out.println(returnvalList.insertReturnval(new Returnval("secondReturnval", true, "Integer")));
 		System.out.println(returnvalList.getReturnval("firstReturnval").getName());
+		
+		IFunctionHeadSlot headSlot = new FunctionHeadSlot(3,3);
+		
+		System.out.println(headSlot.insertExcutee(excutee));
+		System.out.println(headSlot.insertExcutee(new NormalExcutee("thirdExcutee", null, 0)));
+		System.out.println(headSlot.getExcutee("firstExcutee").getName());
+		System.out.println(headSlot.insertParameter(parameter));
+		System.out.println(headSlot.insertParameter(new Parameter("thirdParameter", true, "Integer")));
+		System.out.println(headSlot.getParameter("firstParameter").getReturnval().getName());
+	
+		IFunctionRearSlot rearSlot = new FunctionRearSlot(3,3);
+
+		System.out.println(rearSlot.insertExcuter(excuter));
+		System.out.println(rearSlot.insertExcuter(new Excuter("thirdExcuter")));
+		System.out.println(rearSlot.getExcuter("firstExcuter").getExcutee().getName());
+		System.out.println(rearSlot.insertReturnval(returnval));
+		System.out.println(rearSlot.insertReturnval(new Returnval("thirdReturnval", true, "Integer")));
+		System.out.println(rearSlot.getReturnval("firstReturnval").getName());
 		
 	}
 
