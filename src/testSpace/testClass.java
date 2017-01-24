820137b7d90d1d3a5aedaf3e656c1f00ba420bcf
181847 <753090084@qq.com>
2017-01-24 21:20:29 +0800

    增加对ExcuterList/ParameterList/ReturnvalList的测试用例

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -37,13 +37,27 @@ public class testClass {
 		
 		ExcuteeList excuteeList = new ExcuteeList(3);
 		
-		System.out.println(excuteeList.addExcutee(excutee));
-		System.out.println(excuteeList.addExcutee(new NormalExcutee("secondExcutee", null, 0)));
+		System.out.println(excuteeList.insertExcutee(excutee));
+		System.out.println(excuteeList.insertExcutee(new NormalExcutee("secondExcutee", null, 0)));
 		System.out.println(excuteeList.getExcutee("firstExcutee").getName());
 		
+		ExcuterList excuterList = new ExcuterList(3);
 		
+		System.out.println(excuterList.insertExcuter(excuter));
+		System.out.println(excuterList.insertExcuter(new Excuter("secondExcuter")));
+		System.out.println(excuterList.getExcuter("firstExcuter").getExcutee().getName());
 		
+		ParameterList parameterList = new ParameterList(3);
 		
+		System.out.println(parameterList.insertParameter(parameter));
+		System.out.println(parameterList.insertParameter(new Parameter("secondParameter", true, "Integer")));
+		System.out.println(parameterList.getParameter("firstParameter").getReturnval().getName());
+		
+		ReturnvalList returnvalList = new ReturnvalList(3);
+		
+		System.out.println(returnvalList.insertReturnval(returnval));
+		System.out.println(returnvalList.insertReturnval(new Returnval("secondReturnval", true, "Integer")));
+		System.out.println(returnvalList.getReturnval("firstReturnval").getName());
 		
 	}
 
