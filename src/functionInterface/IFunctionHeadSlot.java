61fa28003876e28196616365e68420a487a79feb
181847 <753090084@qq.com>
2017-01-24 21:28:04 +0800

    去掉IFunctionHeadSlot中的方法，改为直接继承IExcuteeList和IParameterList中的方法； 去掉IFunctionRearSlot中的方法，改为直接继承IExcuterList和IReturnvalList中的方法

--- src/functionInterface/IFunctionHeadSlot.java
+++ src/functionInterface/IFunctionHeadSlot.java
@@ -1,19 +1,5 @@
 package functionInterface;
 
-public interface IFunctionHeadSlot {
-	//添加Excutee，用int返回添加的结果，成功返回1
-	public int addExcutee(String excuteeName, int paragraphToFire, int nextExcuterInRearSlot);
-	//查找Excutee
-	public IExcutee getExcutee(String excuteeName);
-	public IExcutee getExcutee(int excuteeNumber);
-	//删除Excutee
-	public int removeExcutee(String excuteeName);
+public interface IFunctionHeadSlot extends IExcuteeList, IParameterList{
 	
-	//添加Parameter，用int返回添加的结果，成功返回1
-	public int addParameter(String ParameterName, String rClass);
-	//查找Parameter
-	public IParameter getParameter(String parameterName);
-	public IParameter getParameter(int parameterNumber);
-	//删除Parameter
-	public int removeParameter(String parameterName);
 }
--- src/functionInterface/IFunctionRearSlot.java
+++ src/functionInterface/IFunctionRearSlot.java
@@ -1,19 +1,5 @@
 package functionInterface;
 
-public interface IFunctionRearSlot {
-	//添加Excuter，用int返回添加的结果，成功返回1
-	public int addExcuter(String excuterName);
-	//查找Excuter
-	public IExcuter getExcuter(String excuterName);
-	public IExcuter getExcuter(int excuterNumber);
-	//删除Excuter
-	public int removeExcuter(String excuterName);
+public interface IFunctionRearSlot extends IExcuter, IReturnval{
 	
-	//添加Returnval，用int返回添加的结果，成功返回1
-	public int addReturnval(String returnvalName, String rClass);
-	//查找Parameter
-	public IReturnval getReturnval(String returnvalName);
-	public IReturnval getReturnval(int returnvalNumber);
-	//删除Parameter
-	public int removeReturnval(String returnvalName);
 }
