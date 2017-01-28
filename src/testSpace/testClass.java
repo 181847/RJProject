51e03023a2345b515536188dbfb85d9a0285523a
181847 <753090084@qq.com>
2017-01-28 16:48:50 +0800

    根据之前对于获取Excutee\Parameter\Excuter\Returnval的方法名的更改，对测试用例进行修改

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -1,15 +1,12 @@
 package testSpace;
-import rClassInterface.*;
 import functionInterface.*;
 
 public class testClass {
 
 	public static void main(String[] args) {
 		IFunction function = new HelloWorldFunction();
-		IExcutee excuteeForFire = function.getExcutee("fire");
-		excuteeForFire.fire();
-		excuteeForFire = function.getExcutee("fireSlot2");
-		excuteeForFire.fire();
+		function.Excutee("fire").fire();
+		function.Excutee("fireSlot2").fire();
 	}
 
 }
