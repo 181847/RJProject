f6fdd8418fa8678cac08011c3ff9ff53cfdbf082
181847 <753090084@qq.com>
2017-01-30 19:52:33 +0800

    升级HelloWorldFunction的测试，添加新的插口以及运行方法
    添加的插口：
    Excutee：tryPara&Retu-用来发动一段运算功能代码，对参数以及返回值进行测试
    Parameter：Integer n；Character ch；两个参数，分别存储整型以及字符型
    Excuter：endTry-用于在发动Excutee tryPara&Retu之后要用到的Excuter
    Returnval：Integer return：整型返回值，按计划是在运算功能之后存储之前的Parameter n加一后的值
    添加一个私有方法myInnerFunc()，用来打包Excutee
    tryPara&Retu:所要运行的代码段，run方法添加代码段3，用来对性tryPara&Retu

--- src/testSpace/HelloWorldFunction.java
+++ src/testSpace/HelloWorldFunction.java
@@ -1,11 +1,26 @@
 package testSpace;
+import function.AbstractFunction;
 import functionInterface.IExcuter;
+import function.LinerExcutee;
+import function.Parameter;
+import function.Excuter;
+import function.Returnval;
 
 public class HelloWorldFunction extends AbstractFunction {
 	public HelloWorldFunction(){
 		super(2, 0, 0, 0);
+		//创建Excutee
 		insertExcutee(new LinerExcutee("fire", 1));
 		insertExcutee(new LinerExcutee("fireSlot2", 2));
+		insertExcutee(new LinerExcutee("tryPara&Retu", 3));
+		//创建Parameter
+		insertParameter(new Parameter("Integer", "n"));
+		insertParameter(new Parameter("Character", "ch"));
+		//创建Excuter
+		insertExcuter(new Excuter("endTry"));
+		//创建Returnval
+		insertReturnval(new Returnval("Integer", "return"));
+		//设置Function的名字
 		setName("Hello World Function");
 	}
 
@@ -19,9 +34,22 @@ public class HelloWorldFunction extends AbstractFunction {
 		case 2:
 			System.out.println("fireSlot2 fired");
 			return null;
+		case 3:
+			return myInnerFun();
 		default:
 			return null;
 		}
+	}
+	
+	private IExcuter myInnerFun(){
+		int n = (Integer)Parameter("n").readObject();
+		char ch = (Character)Parameter("ch").readObject();
+		
+		System.out.println("参数n：" + n + " 参数ch：" + ch);
+		
+		Returnval("return").writeObject(new Integer(n + 1), "Integer");
+		
+		return Excuter("endTry");
 	}
 
 	@Override
