2001fae3054d299ca654ec1eb61822994031242b
181847 <753090084@qq.com>
2017-01-30 19:54:22 +0800

    测试修改后的HelloWorldFunction
    为整型参数赋值45，为字符型参数赋值‘@’，添加第三代码段的运行，最后输出整型返回值

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -5,8 +5,15 @@ public class testClass {
 
 	public static void main(String[] args) {
 		IFunction function = new HelloWorldFunction();
+		
+		function.Parameter("n").writeObject(new Integer(45), "Integer");
+		function.Parameter("ch").writeObject(new Character('@'), "Character");
+		
 		function.Excutee("fire").fire();
 		function.Excutee("fireSlot2").fire();
+		function.Excutee("tryPara&Retu").fire();
+		
+		System.out.println((Integer)function.Returnval("return").readObject());
 	}
 
 }
