ae9b0e031e5d0298b5e223657bbfe42043671a98
181847 <753090084@qq.com>
2017-01-31 14:46:07 +0800

    用FunctionTester测试HelloWorldFunction

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -9,11 +9,7 @@ public class testClass {
 		function.Parameter("n").writeObject(new Integer(45), "Integer");
 		function.Parameter("ch").writeObject(new Character('@'), "Character");
 		
-		function.Excutee("fire").fire();
-		function.Excutee("fireSlot2").fire();
-		function.Excutee("tryPara&Retu").fire();
-		
-		System.out.println((Integer)function.Returnval("return").readObject());
+		new FunctionTester(function).test();
 	}
 
 }
