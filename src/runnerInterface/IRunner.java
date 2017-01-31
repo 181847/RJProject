c53cd01f5ab840da2b1a94383df32ad44e3bcfd1
181847 <753090084@qq.com>
2017-01-31 16:17:50 +0800

    IRunner接口继承IExcuteeStack接口，删去原本的pushExcutee()方法

--- src/runnerInterface/IRunner.java
+++ src/runnerInterface/IRunner.java
@@ -1,19 +1,30 @@
 package runnerInterface;
 import functionInterface.IParameterList;
 import functionInterface.IExcutee;
+import basicInterface.IExcuteeStack;
 
-public interface IRunner{
-	//Runner开始执行运行栈内的Excutee
+public interface IRunner extends IExcuteeStack{
+	/**
+	 *Runner开始执行运行栈内的Excutee
+	 */
 	public void run();
-	//设置在Runner当前的运行循环周期内
-	//是否执行当前Excutee的运算功能
-	public void setExcutedableState(boolean isExcutedable);
-	//设置当前Runner是否处于反向遍历参数的状态
-	public void setRetraverseParameterState(boolean isRetraverseParameter);
-	//输入一个参数列表，
-	//向运行栈压入所有与这些参数链接的
-	//BaseCalculatorFunction的Excutee
+	
+	/**
+	 *输入一个参数列表，
+	 *向运行栈压入所有与这些参数链接的
+	 *BaseCalculatorFunction的Excutee
+	 */
 	public void retraverseParameters(IParameterList parameterList);
-	//将Excutee压入Runner的运行栈
-	public void pushExcutee(IExcutee excutee);
+	
+	/**
+	 *设置当前Runner是否处于反向遍历参数的状态
+	 */
+	public void setRetraverseParameterState(boolean isRetraverseParameter);
+	
+	/**
+	 *设置在Runner当前的运行循环周期内
+	 *是否执行当前Excutee的运算功能
+	 */
+	public void setExcutedableState(boolean isExcutedable);
+	
 }
