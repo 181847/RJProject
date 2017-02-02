3bc6ab04b7858b4f17f74864a57c05dc06494c95
181847 <753090084@qq.com>
2017-02-02 21:41:28 +0800

    IExcutee的方法的返回值设为IExcutee方便在Runner当中对同一个Excutee进行连续的调用

--- src/functionInterface/IExcutee.java
+++ src/functionInterface/IExcutee.java
@@ -6,7 +6,7 @@ public interface IExcutee extends INameable{
 	//欢迎Runner的到来，检查参数是否遍历
 	public void welcomeRunner(IRunner runner);
 	//发动hostFunction的运算功能
-	public void fire();
+	public IExcutee fire();
 	//为Runner指明下一个要运行的地方
 	public void sendRunner(IRunner runner);
 	//设定Excutee所从属的Function
