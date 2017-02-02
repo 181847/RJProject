16d24db27d57ebdcbf7dd7ee3023eb5559b0258f
181847 <753090084@qq.com>
2017-02-02 22:16:08 +0800

    新创建一个HelloWorldFunction，将两个Function相连接，测试runner
    将function的Excuter【endTry】连接到function2的Excutee【tryPara&Retu】，将function的参数【return】连接到function2的【n】

--- src/testSpace/testClass.java
+++ src/testSpace/testClass.java
@@ -1,15 +1,25 @@
 package testSpace;
 import functionInterface.*;
+import runnerInterface.IRunner;
 
 public class testClass {
 
 	public static void main(String[] args) {
 		IFunction function = new HelloWorldFunction();
+		IFunction function2 = new HelloWorldFunction();
 		
 		function.Parameter("n").writeObject(new Integer(45), "Integer");
 		function.Parameter("ch").writeObject(new Character('@'), "Character");
 		
-		new FunctionTester(function).test();
+		
+		function.Excuter("endTry").linkExcutee(function2.Excutee("tryPara&Retu"));
+		function2.Parameter("n").linkReturnval(function.Returnval("return"));
+		
+		IRunner runner = new Runner("LiuXiang");
+		runner.pushExcutee(function.Excutee("tryPara&Retu"));
+		
+		runner.setRunable(true);;
+		runner.run();
 	}
 
 }
