4a33420a1df876bdd671a7ebe8181912dfa65a52
181847 <753090084@qq.com>
2017-02-04 18:21:42 +0800

    增加对ROjbect的测试用例
    从RObject当中获取HelloWorldFunction的一个实例，用FunctionTester对这个function进行测试

---
+++ src/testSpace/TestRClassOfRObject.java
@@ -0,0 +1,24 @@
+package testSpace;
+
+import functionInterface.IFunction;
+import rClassInterface.IRClass;
+import testSpace.testTool.FunctionTester;
+import unfinishedClass.RObject;
+
+public class TestRClassOfRObject {
+	public static void main(String[] args){
+		IRClass rclass = new RObject();
+		IFunction function;
+		
+		try {
+			function = rclass.Function("HelloWorldFunction");
+			new FunctionTester(function).test();
+		} catch (InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
