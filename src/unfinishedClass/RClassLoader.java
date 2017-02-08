6efa70c98c1c4e1e562f76fbe7ab9d29512b51a6
181847 <753090084@qq.com>
2017-02-08 15:30:16 +0800

    完善RClassLoader的成员数据以及成员方法，并且将所有的方法改为非静态
    添加一个RClassIDField：用于存储RClass的Java实例，以及为其中每个的RClass分配一个独一无二的ID，Java包装类型的RClass的ID为正数，完全自定义RClass的ID为负数，ID为0代表空类型。
    
    添加一个Hashtable<String,int>：建立一个所有加载进来的RClass的名字到它的ID的映射，方便通过RClass的名字获得ID，进而再获得RClass的Java实例。
    
    增加成员方法：
    int
    loadJarRClass(IRClass)：将一个RClass的Java实例加载进入idField，如果正常注册了ID，就会进而在Hashtable中增加一对键值（RClass的名字，RClass的ID），返回注册的ID。
    
    int
    loadRClassFromJar(String)：传入的字符串要求必须是一个Jar文件的绝对路径，例如”D:/tempFile/RObject.jar“，本方法从指定的Jar文件中加载RClass类实例，加载前会读取Jar文件中位于
    “RMETA-INF” 文件夹下的
    “RMANIFEST.txt”，这是一个文本文件，在这个文本文件当中要指明这个Jar包当中要加载的RClass的Java类的位置，例如在这个文本文件中写有”MainRClass:
    testSpace.testRClass.RObject“，表明要加载的RClass的Java类是在testSpace.testRClass报下的RObject类，本方法会创建一个URLClassLoader来加载这个类，然后调用之前的int
    loadJarRClass(IRClass)来为RClass的Java实例正式分配ID和建立名字到ID的联系。最后返回分配得到的ID。
    
    int
    loadRClassFromZip(String)：传入的字符串要求必须是一个Zip文件的绝对路径，例如”D:/tempFile/RObject.zip“，这个方法专门用于加载完全自定义的RClass，但是现在还没有对完全自定义RClass进行详细的规划，所以目前这个方法没有任何作用。
    
    int
    loadRClassFrom(String)：传入的字符串要求是一个以“.zip”或者”.jar“结尾的一个文件的绝对路径，然后将不同类型的文件交由不同的方法执行加载任务。
    
    IRClass
    getRClass(String)：先从哈希表当中获得与传入的字符串相匹配的RClassID，然后从idField中找到指定的ID的RClass。
    
    int getRClassIDOf(String)：获得指定名字的RClass的ID，ID直接从哈希表的记录中获取。

--- src/unfinishedClass/RClassLoader.java
+++ src/unfinishedClass/RClassLoader.java
@@ -1,10 +1,165 @@
 package unfinishedClass;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Hashtable;
+
 import rClassInterface.IRClass;
+import rClassInterface.IRClassLoader;
 
-public class RClassLoader{
+public class RClassLoader implements IRClassLoader{	
+	public RClassIDField idField;
+	public Hashtable<String, Integer> nameToID;
+	
+	public RClassLoader(){
+		idField = new RClassIDField();
+		nameToID = new Hashtable<String, Integer>(100, 0.75f);
+	}
+	
+	/**
+	 * 带参构造方法，初始化IDField和Hashtable的空间，
+	 * Hashtable的Capacity为IDField的正负空间数量之和。
+	 * @param positiveSpace IDField的正空间数量，存储基本数据类型RClass以及Java包装类型RClass。
+	 * @param negativeSpace IDField的负空间数量，存储完全自定义RClass。
+	 * @param loadFactor Hashtable的装载因子。
+	 */
+	public RClassLoader(int positiveSpace, int negativeSpace, float loadFactor){
+		idField = new RClassIDField(positiveSpace, negativeSpace);
+		nameToID = new Hashtable<String, Integer>(positiveSpace + negativeSpace, loadFactor);
+	}
+	
+	
+	/**
+	 * 将一个RClass实例对象装载进入RClassLoader，
+	 * 并建立RClass的名字到RClassID的联系。
+	 * @param loadingRClass 要装载的RClass类型对象。
+	 * @return 为RClass注册的RClassID。
+	 */
+	public int loadJarRClass(IRClass rClassToLoad){
+		if (rClassToLoad == null){
+			System.out.println("错误，在方法loadRClass(IRClass) 中，参数rClassToload为null");
+		}
+		
+		System.out.print("已获得RClass实例对象，RClass名字：" + rClassToLoad.getName() + "，准备装载....");
+		int ID = idField.registJarRClass(rClassToLoad);
+		if (ID <= 0){
+			System.out.println("\n注册RClassID失败，RClass名字：" + rClassToLoad.getName());
+			return ID;
+		}
+		System.out.println("装载成功");
+		
+		nameToID.put(rClassToLoad.getName(), ID);
+		return ID;
+	}
+	
+	/**
+	 * 已知文件是jar文件，从中加载RClass，
+	 * 从指定的绝对路径下读取jar文件，
+	 * 加载其中的RClass，这个jar文件中有一个RMETA-INF/RMANIFEST.txt文件，
+	 * 其中有一行记录着MainRClass，冒号空一格之后的信息，
+	 * 就是要从文件中加载RClass路径。
+	 * @param rClassFile 文件的绝对路径。
+	 * @return 加载好了的RClass的ID。
+	 */
+	public int loadRClassFromJar(String rClassPath){
+		String manifestString = "";
+		IRClass rClassToLoad = null;
+		boolean foundMainRClass = false;
+		try{
+			URL url = new URL("file:" + rClassPath);
+			URLClassLoader jarRClassLoader = new URLClassLoader(new URL[]{url}, 
+					Thread.currentThread().getContextClassLoader());
+			
+			InputStream isManifest = jarRClassLoader.getResourceAsStream("RMETA-INF/RMANIFEST.txt");
+			if (isManifest == null){
+				System.out.println("指定的Jar文件中没有找到 RMETA-INF/RMANIFEST.txt，文件路径："+ rClassPath);
+				jarRClassLoader.close();
+				return 0;
+			}
+			BufferedReader brManifest = new BufferedReader(new InputStreamReader(isManifest));
+			
+			while((manifestString = brManifest.readLine()) != null){
+				if (manifestString.startsWith("MainRClass: ")){
+					
+					//12就是字符串 “MainRClass: ” 的长度，注意：包括最后的那个空格
+					manifestString = manifestString.substring(12);
+					
+					Class<?> jarRClass = jarRClassLoader.loadClass(manifestString);
+					rClassToLoad = (IRClass) jarRClass.newInstance();
+					System.out.println(rClassToLoad.getName());
+					
+					foundMainRClass = true;
+					break;
+				}
+			}
+			
+			if (!foundMainRClass){
+				System.out.println("未能发现啊MainRClass信息，请检查jar文件中的RMETA-INF/RMANIFEST.txt是否有“MainRClass: ***.****.**”，文件路径：" + rClassPath);
+			}
+			brManifest.close();
+			jarRClassLoader.close();
+			
+		} catch (MalformedURLException e) {
+			System.out.println("Jar包文件路径不正确，无法创建URL，文件路径：" + rClassPath);
+			e.printStackTrace();
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("没有发现指定的Class，文件路径：" + rClassPath
+					+ "\t将要加载的类名为：" + manifestString);
+			e.printStackTrace();
+			
+		} catch (InstantiationException e) {
+			System.out.println("要加载的类可能为抽象类、接口、数组类、原始类型、void类型，或者该类没有无参构造方法：\n"
+					+ "文件路径：" + rClassPath + "\t要加载的类：" + manifestString);
+			e.printStackTrace();
+			
+		} catch (IllegalAccessException e) {
+			System.out.println("反射创建RClass的Java类实例失败，文件路径：" + rClassPath 
+					+ "\t要加载的类：" + manifestString);
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			System.out.println("RMETA-INF/RMANIFEST.txt文件读取错误，或者文件关闭时发生错误，文件路径：" + rClassPath);
+			e.printStackTrace();
+		}
+		
+		if (foundMainRClass){
+			return loadJarRClass(rClassToLoad);
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * 此方法专门对应完全自定义的RClass，现在未完成，
+	 * 已知文件是zip文件，从中加载RClass。
+	 * @param rClassFile 文件的绝对路径。
+	 * @return 加载好了的RClass的ID。
+	 */
+	public int loadRClassFromZip(String rClassPath){
+		//TODO
+		System.out.println("the method: loadRClassFromZip(), cannot do anything with " + rClassPath);
+		return 0;
+	}
 
-	public static int loadRClass(String rClassPath) {
-		System.out.println("loadRClass() called: did nothing with " + "rClassPath");
+	/**
+	 * 从指定的文件读取rClass，要求文件的名字就是RClass的名字。
+	 * @param rClassPath 文件的绝对路径。
+	 * @return 为RClass注册的RClassID。
+	 */
+	public int loadRClassFrom(String rClassPath) {
+		if (rClassPath.endsWith(".jar"))
+		{
+			return loadRClassFromJar(rClassPath);
+			
+		} else if (rClassPath.endsWith(".zip")){
+			return loadRClassFromZip(rClassPath);
+			
+		}
 		return 0;
 	}
 
@@ -16,7 +171,7 @@ public class RClassLoader{
 	 * @return 现阶段只考虑相等情况，相等返回1；
 	 * 不等返回Integer的最大值。
 	 */
-	public static int checkRClassMatchType(String rClassA, String rClassB) {
+	public int checkRClassMatchType(String rClassA, String rClassB) {
 		if (rClassA.compareTo(rClassB) == 0){
 			return 1;
 		}
@@ -29,12 +184,17 @@ public class RClassLoader{
 	 * @return RClass的类型实例引用。
 	 * 
 	 */
-	public static IRClass getRClass(String rClass) {
-		return null;
+	public IRClass getRClass(String rClass) {
+		Integer returnedID = nameToID.get(rClass);
+		
+		if (returnedID == null){
+			return null;
+		}
+		return idField.getRClass((int)returnedID);
 	}
 	
 	/**
-	 * 查看这个RClass的粗略类型标识，1至9代表基本数据类型。
+	 * 查看这个RClass的注册ID，1至9代表基本数据类型。
 	 * @param rClass rClass的名字。
 	 * @return 粗略类型标识，
 	 * 10以及10以上的是Java包装类；
@@ -42,8 +202,7 @@ public class RClassLoader{
 	 * 0代表空类型；
 	 * 当前只考虑9种基本数据类型。
 	 */
-	public static int getRoughTypeOf(String rClass){
-		
+	public int getRClassIDOf(String rClass){
 		switch(rClass.charAt(0)){
 		case 'B':
 			if (rClass.compareTo("Byte") == 0)
@@ -79,7 +238,12 @@ public class RClassLoader{
 			break;
 		}
 		
-		return 0;
+		Integer returnedID = nameToID.get(rClass);
+		
+		if (returnedID == null){
+			return 0;
+		}
+		return (int)returnedID;
 	}
 
 }
