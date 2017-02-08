6a624adec9ffdb32164447facdd4e07d27d1fe91
181847 <753090084@qq.com>
2017-02-08 15:59:10 +0800

    RClassLoaderManager用来方便代码能够随处获得RClassLoader而设计的
    这个Manager只包含静态成员和静态方法，包括一个RClassLoader静态引用。
    
    静态方法：
    IRClassLoader getRClassLoader()：获得内部的静态成员RClassLoader
    
    void
    prepareRClassLoader()：用于对RClassLoader创建一个实例，以及在其中加载所有的基本数据类型RClass，现在的实现方法只是暂时的，未来将改为从本地的配置文件上读取信息并加载的形式。

---
+++ src/basicInterface/IGetRClassLoader.java
@@ -0,0 +1,6 @@
+package basicInterface;
+import rClassInterface.IRClassLoader;
+
+public interface IGetRClassLoader {
+	public IRClassLoader getRClassLoader();
+}
---
+++ src/unfinishedClass/RClassLoaderManager.java
@@ -0,0 +1,27 @@
+package unfinishedClass;
+import rClassInterface.IRClassLoader;
+import unfinishedClass.basicRClass.*;
+
+public class RClassLoaderManager {
+	public static IRClassLoader rClassLoader;
+	public static int positiveSpace = 100;
+	public static int negativeSpace = 100;
+	public static float loadFactor = 0.75f;
+
+	public static IRClassLoader getRClassLoader(){
+		return rClassLoader;
+	}
+	
+	public static void prepareRClassLoader(){
+		rClassLoader = new RClassLoader(positiveSpace, negativeSpace, loadFactor);
+		rClassLoader.loadJarRClass(new RByte());
+		rClassLoader.loadJarRClass(new RBoolean());
+		rClassLoader.loadJarRClass(new RShort());
+		rClassLoader.loadJarRClass(new RInteger());
+		rClassLoader.loadJarRClass(new RLong());
+		rClassLoader.loadJarRClass(new RFloat());
+		rClassLoader.loadJarRClass(new RDouble());
+		rClassLoader.loadJarRClass(new RCharacter());
+		rClassLoader.loadJarRClass(new RString());
+	}
+}
