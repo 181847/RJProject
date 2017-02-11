1e0c4a0dec55712ff243db61715b188583e7a3a1
181847 <753090084@qq.com>
2017-02-11 15:49:14 +0800

    向IHoldRClassID添加简单的注释

--- src/basicInterface/IHoldRClassID.java
+++ src/basicInterface/IHoldRClassID.java
@@ -1,6 +1,15 @@
 package basicInterface;
 
 public interface IHoldRClassID {
+	/**
+	 * 获取RClassID。
+	 * @return RClassID。
+	 */
 	public int getRClassID();
+	
+	/**
+	 * 设置RClassID。
+	 * @param 要设置的rClassID。
+	 */
 	public void setRClassID(int rClassID); 
 }
