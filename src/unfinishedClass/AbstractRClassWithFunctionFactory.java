e58b70afb3977001042236c396cd5fe12899bd76
181847 <753090084@qq.com>
2017-02-12 14:28:18 +0800

    添加一个增加构造Function的方法，并为其他方法添加注释

--- src/unfinishedClass/AbstractRClassWithFunctionFactory.java
+++ src/unfinishedClass/AbstractRClassWithFunctionFactory.java
@@ -31,11 +31,32 @@ public class AbstractRClassWithFunctionFactory extends AbstractRClass implements
 		return functionFactory.insertFunctionMaker(new FunctionMakerWithClass(functionName, functionClass));	
 	}
 	
+	/**
+	 * 为functionFactory，插入一个FunctionMaker，
+	 * 这里不手动传入Function的名字，而是由程序自动生成一个Function实例，
+	 * 然后从这个新生成的Function实例获得名字，进而设置FunctionMaker的名字。
+	 * @param functionClass 用于直接创建Function的FunctionClass。
+	 * @return 如果已存在相同名字的元素就返回2； 如果插入成功就返回1。
+	 */
 	public int insertFunctionClass(Class<? extends IFunction> functionClass){
 		return functionFactory.insertFunctionMaker(new FunctionMakerWithClass(functionClass));
 		
 	}
 	
+	/**
+	 * 为FunctionFactory，插入一个构造FunctionMaker。
+	 * @param functionClass 一个用于产生构造Function的Function类型。
+	 * @return 成功返回1，如果已存在就返回-1，
+	 * 不对原来的构造Function做任何改动。
+	 */
+	public int insertConstructFunctionClass(Class<? extends IFunction> functionClass){
+		return functionFactory.insertConstructFunctionMaker(new FunctionMakerWithClass(functionClass));
+	}
+	
+	/**
+	 * 获取这个RClass的构造Function。
+	 * @return 一个构造Function实例。
+	 */
 	@Override
 	public IFunction ConstructFunction() {
 		IFunction newFunctionInstance = functionFactory.getConstructFunction();
