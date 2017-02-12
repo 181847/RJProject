0c43cadbb337bdea1099a0660254b72201ca2ae9
181847 <753090084@qq.com>
2017-02-12 20:11:21 +0800

    为IFunctionMaker添加一个接口
    这个接口专门用于脚本定义的Function，用于填充其中的contentGraph

--- src/functionInterface/IFunctionMaker.java
+++ src/functionInterface/IFunctionMaker.java
@@ -12,4 +12,14 @@ public interface IFunctionMaker extends INameable {
 	 * @return 新的Function实例对象。
 	 */
 	public IFunction makeFunction();
+	
+	/**
+	 * 专门适用于完全自定义型RClass的方法，通过读取相应的function描述文件，
+	 * 对CustomFunction的内部进行填充。
+	 * @param rearSlot 某个CustomFunction的内部裸露出来的Returnval和Excuter；
+	 * @param headSlot 某个CustomFunction的内部裸露出来的Excutee和Parameter；
+	 * @return 成功填充返回1，失败返回0。
+	 */
+	public int fillFunctionGraphOf(IFunctionRearSlot rearSlot, IFunctionHeadSlot headSlot);
+
 }
