a12e92ad703aafa124bdf1fc9e275b9a2ad8a3a9
181847 <753090084@qq.com>
2017-02-14 16:48:32 +0800

    添加一种新的RClass，用来包装Exception类型

---
+++ src/unfinishedClass/basicRClass/RException/RException.java
@@ -0,0 +1,12 @@
+package unfinishedClass.basicRClass.RException;
+
+import rClass.AbstractRClassForJava;
+import rClassInterface.IRClass;
+
+public class RException extends AbstractRClassForJava implements IRClass {
+
+	public RException() {
+		super("Exception");
+	}
+
+}
