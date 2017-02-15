b9bc510fd9739804d6042bbd561aa8e0d6aa5278
181847 <753090084@qq.com>
2017-02-15 21:01:17 +0800

    创建一种实现了IParameter和IReturnval的类
    这种类主要用于SentryFunction中用于快速的传递参数，SentryFunction的主要作用是将外部的参数向内部的Function传递，不应该对参数进行任何修改，又因为Parameter和Returnval都是RReference的子类，所以这里从Parameter继承出来，实现IReturnval的接口，这个混合类的实例既可以当作Parameter，又可以当作Returnval，作为RReference读取数据时读取的也是同样的数据，很好的实现了参数的传递。

---
+++ src/unfinishedClass/MixParameterAndReturnval.java
@@ -0,0 +1,42 @@
+package unfinishedClass;
+
+import function.component.Parameter;
+import functionInterface.IExcutee;
+import functionInterface.IParameter;
+import functionInterface.IReturnval;
+
+/**
+ * 这个特殊种类的类既可以当Parameter，
+ * 也可以当作Returnval，
+ * 作参数和返回值时，所包含的数据都是一样的，
+ * 这个类专门用于HeadSentryFunction和RearSentryFunction。
+ * @author 75309
+ *
+ */
+public class MixParameterAndReturnval extends Parameter implements IParameter, IReturnval {
+
+	public MixParameterAndReturnval(String referenceClass, String parameterName) {
+		super(referenceClass, parameterName);
+	}
+	
+	/**
+	 * 这个类作为Returnval的时候并不能使得Runner反向遍历基本运算Function，
+	 * 所以这个方法的返回值一直为null。
+	 */
+	@Override
+	public IExcutee getExcutee() {
+		//Empty Body
+		return null;
+	}
+
+	/**
+	 * 这个类作为Returnval的时候并不能使得Runner反向遍历基本运算Function，
+	 * 所以这个方法有任何实际功能，返回值一直为0。
+	 */
+	@Override
+	public int linkExcutee(IExcutee excutee) {
+		//Empty Body
+		return 0;
+	}
+
+}
