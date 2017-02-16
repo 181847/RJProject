0d41a4eea2047560323e6d128eb406b92ef80971
181847 <753090084@qq.com>
2017-02-16 17:56:28 +0800

    准备空间中多加一个静态成员Runner，方便用Runner进行测试。

--- src/testSpace/Test.java
+++ src/testSpace/Test.java
@@ -2,9 +2,12 @@ package testSpace;
 
 import rClass.RClassLoaderManager;
 import rClassInterface.IRClassLoader;
+import runner.Runner;
+import runnerInterface.IRunner;
 
 public class Test {
 	public static IRClassLoader loader;
+	public static IRunner runner;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,6 +17,9 @@ public class Test {
 	public static void prepare(){
 		RClassLoaderManager.prepareRClassLoader();
 		loader = RClassLoaderManager.getRClassLoader();
+		
+		runner = new Runner("LiuXiang");
+		runner.setRunable(true);
 	}
 
 }
