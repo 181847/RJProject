d3491fc62b2548d7129f863274095e32d98197d7
181847 <753090084@qq.com>
2017-02-16 13:45:06 +0800

    为了让SentryFunction能够将Runner转到异常线路上
    把MixExcuteeAndExcuter的父类改为LiinerExcutee，只实现IExcuter的接口和覆盖sendRunner()，当nextExcuter为null的时候，才将自己当作下一个Excuter，否则按照正常的处理流程传递Runner。

--- src/unfinishedClass/MixExcuteeAndExcuter.java
+++ src/unfinishedClass/MixExcuteeAndExcuter.java
@@ -1,19 +1,16 @@
 package unfinishedClass;
 
-import basicTool.NameableWithString;
+import function.component.LinerExcutee;
 import functionInterface.IExcutee;
 import functionInterface.IExcuter;
 import functionInterface.IFunction;
 import runnerInterface.IRunner;
 
-public class MixExcuteeAndExcuter extends NameableWithString implements IExcutee, IExcuter {
+public class MixExcuteeAndExcuter extends LinerExcutee implements IExcutee, IExcuter {
 	public IExcutee excutee;
-	public boolean isReady;
-	public IFunction hostFunction;
-	public int paragraphToFire;
 	
 	public MixExcuteeAndExcuter(String excuteeName, int paragraphToFire) {
-		super(excuteeName);
+		super(excuteeName, paragraphToFire);
 	}
 
 	/**
@@ -42,66 +39,23 @@ public class MixExcuteeAndExcuter extends NameableWithString implements IExcutee
 	/**
 	 * 由于这个类属于Excutee和Excuter的混合类型，
 	 * 成员变量有一个Excutee（即下一个要发动的对象），
-	 * 这里无需获取IExcuter，直接将成员Excutee压入runner的运行栈内。
+	 * 如果nextExcuter为null，就会把自己当成下一个Excuter，
+	 * 将成员变量excutee压入runner的运行栈，
+	 * 如果nextExcuter有值，就会按照正常流程操作。
+	 * 一般来说这个特殊的Excutee/Excuter只用在SentryFunction中，
+	 * 在SentryFunction中的run()方法一般返回的Excuter是null，
+	 * 只有在发生异常的时候nextExcuter才会被放置到异常线路上，
+	 * 保证即使在SentryFunction中发生的异常也能够被处理。
 	 * @param runner 操作对象Runner
 	 */
 	@Override
 	public void sendRunner(IRunner runner){
-		runner.pushExcutee(excutee);
-		isReady = !hostFunction.needParameters();
-	}
-
-	/**
-	 * fire()方法发动hostFunction的invoke()方法，
-	 * 这里不需要获取下一个Excuter，因为这个Excutee本身就是一个Excuter，
-	 * 相当于这个Excutee自创建以来就已经连接着一个Excuter，
-	 * 在sendRunner()当中直接将自己的一个成员变量excutee压入Runner。
-	 * @return  返回自身的引用，
-	 * 之所以返回自身是为了在Runner当中能够连续执行对于Excutee的方法，
-	 * 详情参考Runner类当中run()方法的内部定义。
-	 */
-	@Override
-	public IExcutee fire() {
-		hostFunction.invoke(paragraphToFire);
-		return this;
-	}
-
-	/**
-	 * 由于这个类既是Excutee也是Excuter，
-	 * 所以调用这个方法返回的就是自身。
-	 * @return 自身。
-	 */
-	@Override
-	public IExcuter getExcuter() {
-		return this;
-	}
-
-	/**
-	 * 返回指定可命名对象的序号。
-	 * @param name 指定的名字
-	 * @return 成功返回大于等于0的数字，
-	 * 失败返回-1。
-	 */
-	@Override
-	public void welcomeRunner(IRunner runner){
-		if (isReady){
-			runner.setExcutedableState(true);
-			runner.setRetraverseParameterState(false);
-		}else{
-			runner.setExcutedableState(false);
-			runner.setRetraverseParameterState(true);
-			runner.retraverseParameters(hostFunction.getParameterList());
-			isReady = true;
+		if (nextExcuter != null){
+			runner.pushExcutee(nextExcuter.getExcutee());
+		} else {
+			runner.pushExcutee(excutee);
 		}
-	}
-
-	/**
-	 * 设定Excutee所从属的Function
-	 * @param hostFunction Excutee所从属的Function对象
-	 */
-	@Override
-	public void setHostFunction(IFunction hostFunction) {
-		this.hostFunction = hostFunction;
+		
 		isReady = !hostFunction.needParameters();
 	}
 }
