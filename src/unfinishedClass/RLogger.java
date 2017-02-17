c96dfab36302afbf7f73158aa1030aef4a854e5a
181847 <753090084@qq.com>
2017-02-17 19:56:01 +0800

    RLogger添加一个静态方法用于处理有关异常的信息

--- src/unfinishedClass/RLogger.java
+++ src/unfinishedClass/RLogger.java
@@ -4,4 +4,8 @@ public class RLogger{
 	public static void log(String message){
 		System.out.println("Logger message: " + message);
 	}
+	
+	public static void logException(Exception e){
+		e.printStackTrace();
+	}
 }
