5b051901c10f58d78d0081547f4d5f638c7672da
181847 <753090084@qq.com>
2017-02-17 19:59:32 +0800

    添加一个重要的概念Info类，抽象类RClassComponentInfo是有关RClass的对象信息集合的父类
    这个Info类型的对象是用来存储有管RReference的信息或者Function的信息，方便根据脚本创建对象的

---
+++ src/unfinishedClass/RClassComponentInfo.java
@@ -0,0 +1,43 @@
+package unfinishedClass;
+
+import rClass.RClassLoaderManager;
+
+public abstract class RClassComponentInfo{
+	public int rClassID;
+	public String rClassName;
+
+	/**
+	 * 设置RClass的名字，
+	 * 与此同时设置rClassID。
+	 * @param rClassName RClass的名字。
+	 */
+	public RClassComponentInfo(String rClassName){
+		setRClassName(rClassName);
+	}
+	
+	/**
+	 * @return rClassID。
+	 */
+	public int getRClassID() {
+		return rClassID;
+	}
+
+	/**
+	 * @return RClass的名字。
+	 */
+	public String getRClassName(){
+		return rClassName;
+	}
+	
+	/**
+	 * 设置信息中的RClass的名字，
+	 * 与此同时也会更新RClassID。
+	 * @param rClassName 新的RClass的名字。
+	 * @return 信息更新之后的RClassID。
+	 */
+	public int setRClassName(String rClassName){
+		this.rClassName = rClassName;
+		rClassID = RClassLoaderManager.getRClassLoader().getRClassIDOf(rClassName);
+		return rClassID;
+	}
+}
