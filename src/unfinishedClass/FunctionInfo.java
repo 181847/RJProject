130dd3039d6cae4a813b5e818ce1cceb8cbaa706
181847 <753090084@qq.com>
2017-02-19 00:11:25 +0800

    FunctionInfo添加定制信息
    就是专门为实现的IModifer接口的Function增加的信息空间，如果当前Function实现了IModifier接口，而且FunctionInfo中的modifierInfo非null/空，就要对当前新生成的Function调用modify(String)方法，进行定制。

--- src/unfinishedClass/FunctionInfo.java
+++ src/unfinishedClass/FunctionInfo.java
@@ -1,5 +1,6 @@
-package unfinishedClass;
+﻿package unfinishedClass;
 
+import basicInterface.IModifier;
 import functionInterface.IFunction;
 import rClass.RClassLoaderManager;
 import rClassInterface.IRClass;
@@ -8,6 +9,7 @@ public class FunctionInfo extends RClassComponentInfo{
 	public String functionName;
 	public int functionIndex;
 	public IRClass rClass;
+	public String modifierInfo;
 
 	/**
 	 * @param rClassName 这个Function所属的RClass的名字。
@@ -15,16 +17,62 @@ public class FunctionInfo extends RClassComponentInfo{
 	 */
 	public FunctionInfo(String rClassName, String functionName) {
 		super(rClassName);
-		setFunctionName(functionName);
+		switch( setFunctionName(functionName) ){
+		case -1:
+			RLogger.log("从rClass获取Function的序号失败，当前Function信息不正确，请检查："
+					+ "当前FunctionInfo对象中的信息：\n"
+					+ "rClassName: " + rClassName
+					+ "rClassID: " + rClassID
+					+ "functionName: " + functionName);
+		}
+	}
+	
+	/**
+	 * 创建一个带有定制信息的Function。
+	 * @param rClassName 这个Function所属的RClass的名字。
+	 * @param functionName 这个Function的名字。
+	 * @param modifierInfo 定制Function的信息。
+	 */
+	public FunctionInfo(String rClassName, String functionName, String modifierInfo){
+		this(rClassName, functionName);
+		switch( setModiferInfo(modifierInfo) ){
+		case 0:
+			RLogger.log("ModifierInfo（定制Function信息）设置失败，modifierInfo参数为null，请检查："
+					+ "当前FunctionInfo对象中的信息：\n"
+					+ "rClassName: " + rClassName
+					+ "rClassID: " + rClassID
+					+ "functionName: " + functionIndex);
+		}
+	}
+	
+	/**
+	 * 设置modifierInfo（定制信息）。
+	 * @param modifierInfo 定制Function所需要的特定规范的字符串。
+	 * @return modifierInfo为null返回0；
+	 * 否则返回1。
+	 */
+	public int setModiferInfo(String modifierInfo){
+		if (modifierInfo == null){
+			return 0;
+		}
+		this.modifierInfo = modifierInfo;
+		return 1;
 	}
 	
 	/**
 	 * 设置Funciton的名字，
 	 * 同时也会更新functionIndex。
 	 * @param functionName
-	 * @return
+	 * @return 如果名字为null，返回-2。
+	 * 如果成功在rClass中找到相应的Function；
+	 * 返回这个Function的序号（大于等于0的正整数）；
+	 * 如果没有找到Function，
+	 * 返回-1。
 	 */
 	public int setFunctionName(String functionName){
+		if (functionName == null){
+			return -2;
+		}
 		this.functionName = functionName;
 		rClass = RClassLoaderManager.
 							getRClassLoader().
@@ -58,8 +106,29 @@ public class FunctionInfo extends RClassComponentInfo{
 	 */
 	public IFunction getInstance() {
 		if (rClass != null){
-			return rClass.getFunction(functionIndex);
+			IFunction newFunction = rClass.getFunction(functionIndex);
+			
+			if (newFunction == null){
+				RLogger.log("从FunctionInfo中获取Function实例失败，请检查："
+					+ "当前FunctionInfo对象中的信息：\n"
+					+ "rClassName: " + rClassName
+					+ "rClassID: " + rClassID
+					+ "functionName: " + functionIndex
+					+ "modifierInfo: " + modifierInfo);
+			}
+			if (newFunction instanceof IModifier 
+				&& modifierInfo != null 
+				&& !modifierInfo.isEmpty() ){
+				((IModifier) newFunction).modify(modifierInfo);
+			}
+			return newFunction;
 		}
+		RLogger.log("从FunctionInfo中获取Function实例失败，rClass对象为null，请检查："
+					+ "当前FunctionInfo对象中的信息：\n"
+					+ "rClassName: " + rClassName
+					+ "rClassID: " + rClassID
+					+ "functionName: " + functionIndex
+					+ "modifierInfo: " + modifierInfo);
 		return null;
 	}
 }
