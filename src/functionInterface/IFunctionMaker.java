7d235670f35bd1e31d30ae738ee862f30a36c877
181847 <753090084@qq.com>
2017-02-19 20:48:08 +0800

    IFunctionMaker添加继承的接口IRClassIDHolder
    使得FunctionMaker能够存储RClassID

--- src/functionInterface/IFunctionMaker.java
+++ src/functionInterface/IFunctionMaker.java
@@ -1,12 +1,13 @@
 package functionInterface;
 
 import basicInterface.INameable;
+import basicInterface.IRClassIDHolder;
 
 /**
  * IFunctionMaker是一个可命名对象，
  * 它的名字和它所要创造的function的名字相同。
  */
-public interface IFunctionMaker extends INameable {
+public interface IFunctionMaker extends INameable,IRClassIDHolder{
 	/**
 	 * 创建一个新的Function实例对象。
 	 * @return 新的Function实例对象。
