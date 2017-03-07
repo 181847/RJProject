669ad691c3bab33c673e0176bba9077f9d302f59
181847 <753090084@qq.com>
2017-03-07 21:26:33 +0800

    在FunctionFactory和AbstractRClassWithFunctionFactory中实现有关填充FunctionGraph的方法。
    讲方法的具体实现交个FunctionMaker执行。

--- src/function/tool/FunctionFactory.java
+++ src/function/tool/FunctionFactory.java
@@ -1,6 +1,7 @@
 package function.tool;
 
 import basicTool.NamedItemList;
+import basicTool.RLogger;
 import functionInterface.IFunction;
 import functionInterface.IFunctionFactory;
 import functionInterface.IFunctionMaker;
@@ -44,5 +45,31 @@ public class FunctionFactory extends NamedItemList implements IFunctionFactory {
 	@Override
 	public int insertFunctionMaker(IFunctionMaker functionMaker) {
 		return insertItem(functionMaker);
+	}
+
+	@Override
+	public int fillFunctionGraphOf(String functionName, 
+			IFunction headSentryFunction, 
+			IFunction rearSentryFunction) {
+		IFunctionMaker currentMaker = (IFunctionMaker)getItem(functionName);
+		if (currentMaker == null){
+			RLogger.logError("FunctionFactory 填充CustomFunction出错，"
+					+ "没有找到指定名字的FunctionMaker，请检查："
+					+ "CustomFunction的名字：" + functionName);
+		}
+		
+		return currentMaker.fillFunctionGraphOf(headSentryFunction, rearSentryFunction);
+	}
+
+	@Override
+	public int fillFunctionGraphOf(int functionIndex, IFunction headSentryFunction, IFunction rearSentryFunction) {
+		IFunctionMaker currentMaker = (IFunctionMaker)getItem(functionIndex);
+		if (currentMaker == null){
+			RLogger.logError("FunctionFactory 填充CustomFunction出错，"
+					+ "没有找到指定编号的FunctionMaker，请检查："
+					+ "FunctionFactory的元素数量：" + getNum()
+					+ "CustomFunction的编号：" + functionIndex);
+		}
+		return currentMaker.fillFunctionGraphOf(headSentryFunction, rearSentryFunction);
 	}
 }
--- src/rClass/AbstractRClassWithFunctionFactory.java
+++ src/rClass/AbstractRClassWithFunctionFactory.java
@@ -1,11 +1,10 @@
 package rClass;
 
+import basicTool.RLogger;
 import function.tool.FunctionFactory;
 import functionInterface.IFunction;
 import functionInterface.IFunctionFactory;
-import functionInterface.IFunctionHeadSlot;
 import functionInterface.IFunctionMaker;
-import functionInterface.IFunctionRearSlot;
 import rClassInterface.IRClass;
 import rClassInterface.IRReference;
 
@@ -18,6 +17,7 @@ public abstract class AbstractRClassWithFunctionFactory extends AbstractRClass i
 	public IFunctionFactory functionFactory;
 
 	/**
+	 * 
 	 * @param rClassName RClass的名字。
 	 * @param factorySpace functionFactory初始的空间数量，
 	 * 虽说空间会在不够的时候自动扩容，
@@ -31,13 +31,74 @@ public abstract class AbstractRClassWithFunctionFactory extends AbstractRClass i
 	@Override
 	public abstract IRReference getNewInstance();
 	
+	/**
+	 * 专门适用于完全自定义型RClass的方法，通过名字读取相应的function描述文件，
+	 * 通过customFunction的名字，
+	 * 来填充自定义customFunction的内容图表。
+	 * @param funcitonName
+	 * 		customFunction的名字。
+	 * @param headSentryFunction
+	 * 		customFunction的headSentryFunction。
+	 * @param rearSentryFunction
+	 * 		customFunction的rearSentryFunction。
+	 * @return
+	 * 		functionName为null或者空串，返回0；
+	 * 		两个Function参数为null返回-1；
+	 * 		成功返回1。
+	 */
 	@Override
-	public abstract int fillFunctionGraphOf(String functionName, 
-			IFunctionRearSlot rearSlot, IFunctionHeadSlot headSlot);
+	public int fillFunctionGraphOf(String functionName,
+			IFunction headSentryFunction,
+			IFunction rearSentryFunction){
+		
+		if (functionName == null ||
+				functionName.isEmpty()){
+			RLogger.logError("RClass填充functionGraph出错，functionName为null或空串。");
+			return 0;
+		}//if
+		if (headSentryFunction == null ||
+				rearSentryFunction == null){
+			RLogger.logError("RClass填充functionGraph出错，"
+					+ "headSentryFunction或rearSentryFunction为null。");
+			return -1;
+		}//if
+		return functionFactory.fillFunctionGraphOf(functionName, 
+				headSentryFunction, rearSentryFunction);
+	}
 
+	/**
+	 * 专门适用于完全自定义型RClass的方法，通过序号读取相应的function描述文件，
+	 * 通过customFunction的序号，
+	 * 来填充自定义customFunction的内容图表。
+	 * @param funcitonName
+	 * 		customFunction的名字。
+	 * @param headSentryFunction
+	 * 		customFunction的headSentryFunction。
+	 * @param rearSentryFunction
+	 * 		customFunction的rearSentryFunction。
+	 * @return
+	 * 		functionIndex小于0，返回0；
+	 * 		两个Function参数为null返回-1；
+	 * 		成功返回1。
+	 */
 	@Override
-	public abstract int fillFunctionGraphOf(int functionIndex, 
-			IFunctionRearSlot rearSlot, IFunctionHeadSlot headSlot);
+	public int fillFunctionGraphOf(int functionIndex,
+			IFunction headSentryFunction,
+			IFunction rearSentryFunction){
+		
+		if (functionIndex < 0){
+			RLogger.logError("RClass填充functionGraph出错，functionName为null或空串。");
+			return 0;
+		}//if
+		if (headSentryFunction == null ||
+				rearSentryFunction == null){
+			RLogger.logError("RClass填充functionGraph出错，"
+					+ "headSentryFunction或rearSentryFunction为null。");
+			return -1;
+		}//if
+		return functionFactory.fillFunctionGraphOf(functionIndex, 
+				headSentryFunction, rearSentryFunction);
+	}
 	
 	/**
 	 * 向FunctionFactory插入一个FunctionMaker。
@@ -49,6 +110,13 @@ public abstract class AbstractRClassWithFunctionFactory extends AbstractRClass i
 		return functionFactory.insertFunctionMaker(functionMaker);
 	}
 
+	/**
+	 * 获取指定名字的Function（或者FunctionMaker）的序号，
+	 * @param functionName
+	 * 		Function（或者FunctionMaker）的名字。
+	 * @return
+	 * 		Function（或者FunctionMaker）的序号
+	 */
 	@Override
 	public int getFunctionIndexOf(String functionName) {
 		return functionFactory.getFunctionIndexOf(functionName);
