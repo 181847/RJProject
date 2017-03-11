00a49f8e2cd1d647bfa99413d9e51c49493e39ad
181847 <753090084@qq.com>
2017-03-11 18:05:24 +0800

    创建CustomRClass类，继承AbstractRClassWithFunctionFacto。y
    用两个RReferenceInfo数组分别存储一个CustomRClass增加的静态成员的普通成员。
    IRReference members，用来存储这个类的一个临时的引用模板，这个模板的作用主要有两个：
    1. 存储静态成员，包括父类的静态成员；
    2. 记录本类的各个成员的顺序，方便Getter获取指定名字的成员。
    
    customFunctionFactory，这个FunctionFactory需要能够存储同名的FunctionMaker，用来方便Function的多态调用，它需要配合functionOffsetIndex来配合使用，一个RClass会继承很多接口和类，每一个接口和父类（包括父类的父类）的FunctionMaker都会以他们在自己的FunctionFactory中的顺序在本RClass的FunctionFactory中的一块连续空间进行存储，每个不同的接口和父类占用的空间各不一样，但由于可能会有Function的覆盖等性质，会使得不同空间位置的FunctionMaker引用指向同一个实例，通过这种覆盖来实现调用接口方法的时候，根据实例的实现不同来发动不同的功能。综上，这个FunctionFactory需要允许同名FunctionMaker的存在。
    
    而functionOffsetIndex就存储者本RClass的所有层次的接口和父类的FunctionMaker在此Factory当中的序号的位置。

---
+++ src/unfinishedClass/customRClass/CustomRClass.java
@@ -0,0 +1,74 @@
+package unfinishedClass.customRClass;
+
+import function.tool.FunctionFactory;
+import functionInterface.IFunctionMaker;
+import rClass.AbstractRClassWithFunctionFactory;
+import rClass.RClassLoaderManager;
+import rClassInterface.IRReference;
+import unfinishedClass.FunctionInfo;
+import unfinishedClass.RReferenceInfo;
+
+public class CustomRClass extends AbstractRClassWithFunctionFactory{
+	/**
+	 * 存有这个RClass新增的静态成员的信息。
+	 */
+	protected RReferenceInfo[] staticMembers;
+	
+	/**
+	 * 存有这个RClass的新增的普通成员的信息。
+	 */
+	protected RReferenceInfo[] normalMembers;
+
+	/**
+	 * 存有这个RClass的静态成员的RReference。
+	 */
+	protected IRReference members;
+	
+	/**
+	 * 成员Function偏移指针索引，
+	 * 这个RClass所有的父类、接口，
+	 * 都会在这个偏移索引中拥有一个整形数组记录，
+	 * 这个记录指向FunctionFactory中的一个位置，
+	 * 从这个位置开始都是这个父类、接口的成员方法。
+	 */
+	protected OffsetIndex functionOffsetIndex;
+	
+	/**
+	 * 这个FunctionFactory用来存储functionMaker，
+	 * 这个FunctionFactory需要特殊设计，
+	 * 为了对应覆盖Function的功能，
+	 * 允许同名FunctionMaker的存在。
+	 */
+	protected CustomFunctionFactory customFunctionFactory;
+	
+	/**
+	 * @return
+	 * 		获取包含成员的RReference对象。
+	 */
+	public IRReference getMembers() {
+		return members;
+	}
+
+	public CustomRClass(String rClassName, int factorySpace) {
+		super(rClassName, factorySpace);
+	}
+
+	/**
+	 * 此方法为空，无任何作用。
+	 * 只有用Java定义的RClass需要通过这个方法来加载所有的FunctionMaker，
+	 * CustomRClass不需要这个方法来加载FunctionMaker，
+	 * 它加载的FunctionMaker是特殊的，
+	 * 需要借助脚本文件来创建对应的FunctionMaker。
+	 */
+	@Override
+	public int loadFunction() {
+		//Empty Body
+		return 0;
+	}
+
+	@Override
+	public IRReference getNewInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
