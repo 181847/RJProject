73fa15f842ec8ee20d14bc7de9ffd88a73aa6e09
181847 <753090084@qq.com>
2017-03-29 23:44:29 +0800

    RLogger增加输出错误信息的方法，使得错误信息更加规范。

--- src/basicTool/RLogger.java
+++ src/basicTool/RLogger.java
@@ -6,10 +6,23 @@ public class RLogger{
 	}
 	
 	public static void logException(Exception e){
+		System.out.println("Occured Exception: ");
 		e.printStackTrace();
+	}
+	
+	public static void logException(Exception e, String location, String error, String reason){
+		System.out.println("ERROR Location: " + location);
+		logException(e);
+		logError(error, reason);
 	}
 	
 	public static void logError(String message){
 		System.out.println("ERROR! Message: " + message);
+	}
+	
+	public static void logError(String location, String error, String reason){
+		System.out.println("ERROR Location: " + location 
+				+ "\nERROR: " + error
+				+ "\nReason: " + reason);
 	}
 }
