dd34f2028ad3e8a2f71f16ad6b308348fb2dbcb0
181847 <753090084@qq.com>
2017-04-03 23:44:24 +0800

    ScriptSequenceInformation专门作为加载序列的头部，增加实现InformationTy的e的方法。

--- src/unfinishedClass/customRClass/scriptBlock/ScriptSequenceInformation.java
+++ src/unfinishedClass/customRClass/scriptBlock/ScriptSequenceInformation.java
@@ -1,10 +1,23 @@
 package unfinishedClass.customRClass.scriptBlock;
 
 import unfinishedClass.customRClass.scriptBlock.information.Information;
+import unfinishedClass.customRClass.scriptBlock.information.InformationType;
 
 public class ScriptSequenceInformation implements Information {
 	@Override
 	public String toString(){
 		return "ScriptSequence";
+	}
+
+	@Override
+	public InformationType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setType(InformationType type) {
+		// TODO Auto-generated method stub
+		
 	}
 }
