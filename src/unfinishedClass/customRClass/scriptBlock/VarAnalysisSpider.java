b4b7b0f4d881f8fc83461a1bbe568c373cc39a5f
181847 <753090084@qq.com>
2017-04-19 00:46:38 +0800

    为了使VarAnalysisSpider的使用范围更大而修改。
    Function的Parameter和Returnval组件的检查过程和VarAnalysisSpider的检查非常相似，但是赋予的InformationType不一致，为了使VarAS的适用返回更广，将VarAS赋予的InformationType修改成一个成员变量来存储，程序员可以再构造方法中修改这个成员来赋予VAR意外的InformationType。

--- src/unfinishedClass/customRClass/scriptBlock/VarAnalysisSpider.java
+++ src/unfinishedClass/customRClass/scriptBlock/VarAnalysisSpider.java
@@ -5,10 +5,29 @@ import unfinishedClass.customRClass.scriptBlock.information.Information;
 import unfinishedClass.customRClass.scriptBlock.information.InformationType;
 import unfinishedClass.customRClass.scriptBlock.spider.AbstractBCSpider;
 
+/**
+ * 分析变量的Spider，
+ * 本Spider由于可以分析两个字段的变量，
+ * 即“类型 + 名字”这种形式的声明，
+ * 所以这个Spider还会用来检查Function的Parameter和Returnval组件声明，
+ * 但是由于Paramter和Returnval组件的InformationType不是VAR，
+ * 所以这里用一个成员变量 assignedType 来
+ * 记录对合法Information所赋予的类型枚举变量，
+ * 默认为VAR，
+ * 如果有特殊要去可以调用另外一个构造方法来修改赋予的类型。
+ */
 public class VarAnalysisSpider extends AbstractBCSpider {
+	protected InformationType assignedType;
 
 	public VarAnalysisSpider(ScriptBlock targetBlock) {
 		super(targetBlock);
+		assignedType = InformationType.VAR;
+	}
+	
+	public VarAnalysisSpider(ScriptBlock targetBlock,
+			InformationType assignedType){
+		super(targetBlock);
+		this.assignedType = assignedType;
 	}
 
 	@Override
@@ -17,7 +36,7 @@ public class VarAnalysisSpider extends AbstractBCSpider {
 		String informationString = information.getOriginalString();
 		
 		if (RStringChecker.checkVar(informationString)){
-			information.setType(InformationType.VAR);
+			information.setType(assignedType);
 		} else {
 			 //包含非法字符的信息，或者不符合变量声明格式
 			 information.setType(InformationType.VOID);
