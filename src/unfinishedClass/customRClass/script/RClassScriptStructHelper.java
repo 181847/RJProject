b3740f94879a5ea444e7fa96959727ed6cf8f70d
181847 <753090084@qq.com>
2017-05-11 22:53:24 +0800

    删除没用的类。
    这些是原来的RClassScriptStruct类和其他相关类，现在已经被RClassStruct取代了，删除掉。

--- src/unfinishedClass/customRClass/script/RClassScriptStructHelper.java
+++
@@ -1,9 +0,0 @@
-package unfinishedClass.customRClass.script;
-
-import basicTool.RLogger;
-
-public class RClassScriptStructHelper {
-	public static void showScript(RClassScriptStruct scriptStruct){
-		RLogger.log();
-	}
-}
