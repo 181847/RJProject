f10804a8e7c0063c5f1607081364fed23d5bb730
181847 <753090084@qq.com>
2017-05-20 22:28:04 +0800

    修改脚本（弧线部分）分析Spider。
    弧线分析部分原本会把弧线类型声明给剔除掉，这会导致问题，因为在后续的信息提取过程中，无法区分弧线的类型，在这里修改一下，在分析完弧线声明之后，保持弧线信息不变，方便后来提取信息是能够分析弧线类型。

--- src/unfinishedClass/customRClass/scriptBlock/spider/analysisSpider/ArcAnalysisSpider.java
+++ src/unfinishedClass/customRClass/scriptBlock/spider/analysisSpider/ArcAnalysisSpider.java
@@ -18,32 +18,42 @@ public class ArcAnalysisSpider extends AbstractBCSpider {
 		Information information = targetBlock.getInformation();
 		String informationString = information.getOriginalString();
 		
+		//标志检查的位置，
+		//一个完整的弧线声明应该是
+		//“EtoE: 1.funEnd -> 4.construct”
+		//或者“RtoP: 2.result->3.by”
+		//"EtoE: "和“RtoP: ”属于弧线类型声明，
+		//当弧线的类型声明没有问题的时候，只需要检查后面的部分，
+		//所以要对原始的声明信息进行一个分割，
+		//提取出“1.funEnd -> 4.construct”或者“2.result->3.by”，
+		//subLength就是用来存储这个分割位置的变量，
+		//subLength>0 表示弧线类型声明合法，
+		//subLength<0 表示弧线类型声明不合法，也就无需再进一步的检验了，
+		//直接将信息标志为VOID。
+		int subLength;
 		if (informationString.startsWith(ScriptDeclaration.EArc)){
-			//检查执行路线的弧线声明
-			informationString = 
-					informationString.substring(ScriptDeclaration.EArc.length());
-			if (RStringChecker.checkArc(informationString)){
-				information.setType(InformationType.ARC);
-			} else {
-				information.setType(InformationType.VOID);
-				information.appendDescription("EArc弧线声明非法。");
-			}
-			
+			//获取执行弧线声明的长度。
+			subLength = ScriptDeclaration.EArc.length();
 		} else if (informationString.startsWith(ScriptDeclaration.PArc)){
-			//检查参数连接路线的弧线声明
-			informationString = 
-					informationString.substring(ScriptDeclaration.PArc.length());
-			if (RStringChecker.checkArc(informationString)){
-				information.setType(InformationType.ARC);
-			} else {
-				information.setType(InformationType.VOID);
-				information.appendDescription("PArc弧线声明非法。");
-			}
+			//获取执行参数声明的长度。
+			subLength = ScriptDeclaration.PArc.length();
 		} else {
-			information.setType(InformationType.VOID);
-			information.appendDescription("弧线声明解析过程中发现的非弧线声明字段。");
+			//既不是执行弧线声明，
+			//也不是参数弧线声明，
+			//将subLength标志为-1，表示Information不是弧线信息。
+			subLength = -1;
 		}
-
+		
+		//
+		if (subLength > 0 && 
+				RStringChecker.checkArc(
+						informationString.substring(subLength))){
+			information.setType(InformationType.ARC);
+		} else {
+			//记录错误信息。
+			information.setType(InformationType.VOID);
+			information.appendDescription("弧线声明非法。");
+		}
 	}
 
 }
