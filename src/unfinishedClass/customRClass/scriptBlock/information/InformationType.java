901172c241912b26c9c1901045143ecc370f9033
181847 <753090084@qq.com>
2017-06-20 18:42:33 +0800

    InformationType增加三种
    GENERIC_PARAMS表示声明泛参的信息块，该信息块下包含多个泛参信息块。
    
    GEN_PARAM表示一个泛参的信息块，该信息块下包含一个CLASSREF信息块，用于表示泛型约束。
    
    CLASSREF，这种信息块通过树形的结构，表示一个通用的RClass类型，也可以是泛型，例如一个未知类型的泛参T，或者一个简单的String，或者一个MyClass<int,
    String>，或者一个MyClass<M, int>（注意MyClass<M, int>中的M是一个未知类型，即一个泛参）。

--- src/unfinishedClass/customRClass/scriptBlock/information/InformationType.java
+++ src/unfinishedClass/customRClass/scriptBlock/information/InformationType.java
@@ -5,7 +5,9 @@ public enum InformationType {
 	VOID,
 	
 	//以下代表声明标志，并不是实际的脚本内容。
-	TYPE, NAME, EXTENDS, IMPLEMENTS,
+	TYPE, NAME, 
+	GENERIC_PARAMS, //泛参声明
+	EXTENDS, IMPLEMENTS,
 	MEMBER, STATIC, CONFUN, STATICFUN, 
 	FUN, ABSTRACTFUN,
 	
@@ -16,6 +18,13 @@ public enum InformationType {
 	//三种RClass的类型。
 	INTERFACE, ABSTRACT, CLASS,
 	
+	//一个引用RClass的信息块，
+	//即可以表示为一个单独RClass（例如：String），
+	//也可以表示为一个泛型引用
+	//（例如MyClass<T> ，T, MyClass<String>，其中T是一个未知的泛参）。
+	CLASSREF,
+	
+	GEN_PARAM,	//泛参
 	VAR, 		//一个可以带有初始值变量信息
 	CLASSNAME, 	//一个RClass的类名。
 	CONTENT, 	//CONTENT表示实际的脚本内容。
