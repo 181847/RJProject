49e7187162f15b30866af2ae38192375389bd15e
181847 <753090084@qq.com>
2017-07-03 00:19:10 +0800

    为ScriptAnalysisSpider增加功能。
    这个功能就是为StaticFunction、Function、AbstractFunction的命名进行检查，查看是否符合组件命名规范。

--- src/unfinishedClass/customRClass/scriptBlock/spider/analysisSpider/ScriptAnalysisSpider.java
+++ src/unfinishedClass/customRClass/scriptBlock/spider/analysisSpider/ScriptAnalysisSpider.java
@@ -77,33 +77,33 @@ public class ScriptAnalysisSpider extends CountableSpider {
 			}
 					
 		} else if (targetInfoString.equals(ScriptDeclaration.declar_fun_confun)){
-			//构造Function声明，注意构造Function没有Function的名字，
+			//构造Function声明，注意构造Function没有名字，
 			//所以这里查看是否是构造Function信息块声明需要看这个声明字符串是不是
 			//equal(ScriptDeclaration.conFun)，
 			//而不是像下面的其他类型的Function检查声明字符串是不是以相应的声明开头
 			//startsWith(……)。
 			setInfo(InformationType.DECLAR_FUN_CONFUN);
-			//为构造Function声明下面的信息赋予InformationType
+			//分析构造Function
 			analysisFun();
 					
 		} else if (targetInfoString.startsWith(ScriptDeclaration.declar_fun_static)){
-			//静态Function声明
-			setInfo(InformationType.DECLAR_FUN_STATIC);
+			//进一步分析静态Function的名字是否符合命名规范。
+			analysisFunDeclar(
+					ScriptDeclaration.declar_fun_static,
+					InformationType.DECLAR_FUN_STATIC);
 			
-			//为静态Function声明下面的信息赋予InformationType
-			analysisFun();
 					
 		} else if (targetInfoString.startsWith(ScriptDeclaration.declar_fun)){
-			//成员Function声明
-			setInfo(InformationType.DECLAR_FUN);
-			//为成员Function声明下面的信息赋予InformationType
-			analysisFun();
+			//进一步分析Function的名字是否符合命名规范。
+			analysisFunDeclar(
+					ScriptDeclaration.declar_fun,
+					InformationType.DECLAR_FUN);
 					
 		} else if (targetInfoString.startsWith(ScriptDeclaration.declar_fun_abstract)){
-			//抽象Function声明
-			setInfo(InformationType.DECLAR_FUN_ABSTRACT);
-			//为抽象Function声明下面的信息赋予InformationType
-			analysisFun();
+			//进一步分析抽象Function的名字是否符合命名规范。
+			analysisFunDeclar(
+					ScriptDeclaration.declar_fun_abstract,
+					InformationType.DECLAR_FUN_ABSTRACT);
 				
 		} else {
 			//非法信息声明
@@ -111,6 +111,38 @@ public class ScriptAnalysisSpider extends CountableSpider {
 		}
 	}
 	
+	/**
+	 * 此方法单独对当前的targetInfoString进行分析，
+	 * 要求其中 declar + 分割符 后面的字符串符合组件命名规范，
+	 * 如果符合就会设置相应的infoType；
+	 * 如果不符合就会设置空白标签，并追加错误信息。
+	 * 分割符参考ScriptDeclaration.generalSplit。
+	 * 注意，本方法的操作很蠢，
+	 * 直接将targetInfoString剪掉前面对应(declar + 分割符)长度的字符串，
+	 * 对剩余的部分检查是否符合组件命名规范。
+	 * 不保证targetInfoString一定以declar开头。
+	 * @param declar
+	 * 		声明字段，
+	 * 		形如“StaticFunction:”。
+	 * @param infoType
+	 * 		被设置的标签。
+	 */
+	private void analysisFunDeclar(String declar, InformationType infoType) {
+		if (RStringChecker.checkComponentName(	//检查组件命名规范。
+				targetInfoString.substring(
+						//剔除声明字段。
+						declar.length()
+						//剔除分割符。
+						+ ScriptDeclaration.generalSplit.length()))) {
+			//符合组件命名规范，
+			//设置标签。
+			setInfo(infoType);
+		} else {
+			setInfo_VOID();
+			descriptInfo(declar + "之后的部分不满足组件命名规范。");
+		}
+	}
+
 	/**
 	 * 这个方法检查subBlock是存在，
 	 * 如果存在，就进行Function分析。
