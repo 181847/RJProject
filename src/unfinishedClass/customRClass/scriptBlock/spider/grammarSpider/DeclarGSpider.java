8cab0441a4ca7b1fbc6abb5e3b4006ee1ba5cc3a
181847 <753090084@qq.com>
2017-07-07 01:08:48 +0800

    DeclarGSpider的修改
    由于DeclarGSpider检查的对象都是声明字段，不应该在同一个Block链上出现两个相同InfoType的声明字段，所以现在在DeclarGSpider进行语法检查之前，查看是否有相同infoType的声明字段，一旦发现重复，就无需再对其进行语法检查，直接跳过，减少性能损失。

--- src/unfinishedClass/customRClass/scriptBlock/spider/grammarSpider/DeclarGSpider.java
+++ src/unfinishedClass/customRClass/scriptBlock/spider/grammarSpider/DeclarGSpider.java
@@ -6,13 +6,20 @@ import unfinishedClass.customRClass.scriptBlock.ScriptBlock;
  * 这种Spider所检查的信息全部是DECLAR_字段的声明信息，
  * 如果被检查的Block没有subBlock，
  * 则这种Block一定是不符合语法要求的，
- * 不需要再进行任何检查，
- * 此Spider在每次对Block进行检查之前都要先查看是否含有
+ * 而且不允许对重复的声明信息进行检查，
+ * 当发现已经有对先关Block的操作记录时，
+ * 就不会对其他相同信息类型的Block进行操作，
+ * 而是直接跳过，但是要注意，
+ * 即便被跳过，这个Block还是会在记录槽中增加相应的记录，
+ * 跳过的主要目的是减少性能损耗，
+ * 不能拥有重复的声明，
+ * 一旦发现有重复的Block，
+ * 除了第一个之外的其他Block都不需要再进行检查了。
  */
 public abstract class DeclarGSpider extends GrammarSpider {
 
-	public DeclarGSpider(ScriptBlock targetBlock, String spiderDesc) {
-		super(targetBlock, spiderDesc);
+	public DeclarGSpider(ScriptBlock targetBlock, String spiderDesc, int logLength) {
+		super(targetBlock, spiderDesc, logLength);
 	}
 
 	@Override
@@ -20,6 +27,11 @@ public abstract class DeclarGSpider extends GrammarSpider {
 		if ( ! hasSubBlock) {
 			//记录缺少子信息的信息。
 			dealWith_Lack_SubBlock();
+			//记录无意义Block。
+			recordNonesense();
+		} else if ( 1 < getRecordOf(infoType)){
+			//发现重复声明的Block信息类型。
+			dealWith_Unexpected();
 		} else {
 			declarGrammarWork();
 		}
