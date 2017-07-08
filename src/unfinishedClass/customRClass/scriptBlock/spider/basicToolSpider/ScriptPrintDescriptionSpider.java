65df452e856f1ba01916612baf96ca0b6ef081bc
181847 <753090084@qq.com>
2017-07-08 23:32:11 +0800

    修改打印ScriptBlock的PrintSpider的方法实现。
    改变调用的逻辑，方便后期进行修改。

--- src/unfinishedClass/customRClass/scriptBlock/spider/basicToolSpider/ScriptPrintDescriptionSpider.java
+++ src/unfinishedClass/customRClass/scriptBlock/spider/basicToolSpider/ScriptPrintDescriptionSpider.java
@@ -2,31 +2,55 @@ package unfinishedClass.customRClass.scriptBlock.spider.basicToolSpider;
 
 import unfinishedClass.customRClass.scriptBlock.ScriptBlock;
 
+/**
+ * 专门打印一个ScriptBlock子链上的所有信息。
+ */
 public class ScriptPrintDescriptionSpider extends ScriptPrintSpider {
 
 	public ScriptPrintDescriptionSpider(ScriptBlock targetBlock) {
 		super(targetBlock);
 	}
+
+	/**
+	 * 在这个方法中对要打印的信息进行自定义。
+	 */
+	protected void modifyPrintString() {
+		//先调用父类的信息来构造打印字符串。
+		super.modifyPrintString();
+		
+		newLine();
+		
+		//添加标签信息。
+		appendInfoType();
+		
+		//添加子类增加的信息。
+		//换行。
+		newLine();
+		
+		//添加Information的描述。
+		appendDescription();
+	}
 	
-	public ScriptPrintDescriptionSpider(ScriptBlock targetBlock, int startLine, int hierarchy) {
-		super(targetBlock, startLine, hierarchy);
+	/**
+	 * 添加Block中info的标签信息。
+	 */
+	protected void appendInfoType() {
+		printString += 
+				"InfoType: " + infoType.toString();
 	}
 
 	/**
-	 * 在每一行脚本信息的下一行都添加上这个Information的Description。
+	 * 在打印信息的末尾添加Information的描述。
 	 */
-	@Override
-	protected void appendSomeInformation(){
-		printString = printString + 
-				"\nDescription: " + targetBlock.getInformation().getDescription();
+	protected void appendDescription() {
+		printString += 
+				"Desc: " + targetInformation.getDescription();
 	}
-	
+
 	@Override
-	protected void dealWithSubBlock(ScriptBlock subBlock){
-		ScriptPrintSpider newScriptSpider = 
-				new ScriptPrintDescriptionSpider(subBlock, lineCounter, hierarchy + 1);
-		newScriptSpider.workUntilEnd();
-		lineCounter = newScriptSpider.getLineCounter();
+	protected void printSubBlock(){
+		sendSpider(
+				new ScriptPrintDescriptionSpider(subBlock));
 	}
 
 }
--- src/unfinishedClass/customRClass/scriptBlock/spider/basicToolSpider/ScriptPrintSpider.java
+++ src/unfinishedClass/customRClass/scriptBlock/spider/basicToolSpider/ScriptPrintSpider.java
@@ -3,11 +3,26 @@ package unfinishedClass.customRClass.scriptBlock.spider.basicToolSpider;
 import basicTool.RLogger;
 import unfinishedClass.customRClass.scriptBlock.ScriptBlock;
 import unfinishedClass.customRClass.scriptBlock.ScriptDeclaration;
-import unfinishedClass.customRClass.scriptBlock.spider.AbstractBCSpider;
+import unfinishedClass.customRClass.scriptBlock.spider.CountableSpider;
 
-public class ScriptPrintSpider extends AbstractBCSpider {
+/**
+ * 遍历所有的Block，
+ * 打印block中存储的原生脚本信息。
+ */
+public class ScriptPrintSpider extends CountableSpider {
+	/**
+	 * 当前Block链上的Block的层次数。
+	 */
 	protected int hierarchy;
+	
+	/**
+	 * 下一个被打印的Block所属的行数。
+	 */
 	protected int lineCounter;
+	
+	/**
+	 * 单次Block被打印的所有信息的缓冲。
+	 */
 	protected String printString;
 
 	/**
@@ -32,56 +47,84 @@ public class ScriptPrintSpider extends AbstractBCSpider {
 		lineCounter = 1;
 		hierarchy = 0;
 	}
+	
+	/**
+	 * @param hierarchy
+	 * 		设置下一个被打印的层次数。
+	 */
+	public void setHierarchy(int hierarchy) {
+		this.hierarchy = hierarchy;
+	}
 
-	@Override
-	protected void dealWithTargetBlock() {
-		ScriptBlock subBlock = targetBlock.getSub();
-		printString = "";
-		
-		//添加行数信息
-		appendLineCounter();
-		//添加层次信息
-		appendHierarchy();
-		//添加原脚本信息
-		appendOriginalInformation();
-		//其他的增加打印信息的过程，
-		//这个方法主要用来方便子类扩展这个类能够打印的信息。
-		appendSomeInformation();
-		//向RLogger打印printString
-		printString();
-		
-		++lineCounter;
-		if (subBlock != null){
-			dealWithSubBlock(subBlock);
-		}
+	/**
+	 * @param lineCounter
+	 * 		设置下一个被打印的行数。
+	 */
+	public void setLineCounter(int lineCounter) {
+		this.lineCounter = lineCounter;
+	}
+
+	/**
+	 * @return
+	 * 		获取下一个被打印的信息的行数。
+	 */
+	public int getLineCounter() {
+		return lineCounter;
+	}
+
+	/**
+	 * 子类通过修改这个方法来实现对子链的打印，
+	 * 在这个方法中调用sendSpider可以实现对子链的连续打印，
+	 * 比如行数连续，
+	 * 层次会进行划分，
+	 * sendSpider会将传入的sps的行数和层次进行更新，
+	 * 与本链的行数保持连贯，
+	 * 层次进行区分。<br>
+	 * 另外，这个方法只有在包含子链的时候才会调用，
+	 * 子类无需担心调用这个方法是子链是否为空。
+	 */
+	protected void printSubBlock() {
+		//向子链发出另一个ScriptPrintSpider，
+		//继续打印子信息。
+		sendSpider(
+				new ScriptPrintSpider(subBlock));
 	}
 	
 	/**
-	 * 处理下层的Spider。
-	 * @param subBlock
-	 * 		下一层的Spider，
-	 * 		这个subBlock一定不为null，
-	 * 		请放行使用。
+	 * 默认将子链和本链的层次进行区分，
+	 * 行数连贯起来。
+	 * @param sps
+	 * 		已经放置到子链上的ScriptPrintSpider。
 	 */
-	protected void dealWithSubBlock(ScriptBlock subBlock) {
-		ScriptPrintSpider newScriptSpider = 
-				new ScriptPrintSpider(subBlock, lineCounter, hierarchy + 1);
-		newScriptSpider.workUntilEnd();
-		lineCounter = newScriptSpider.getLineCounter();
+	protected void sendSpider(ScriptPrintSpider sps){
+		//设置子链的层次加一。
+		sps.setHierarchy(hierarchy + 1);
+		
+		//设置下一个子链的起始行数计数。
+		sps.setLineCounter(lineCounter);
+		sps.workUntilEnd();
+		lineCounter = sps.getLineCounter();
 	}
 
-	/**
-	 * 向printString的末尾添加targetBlock.information.getOriginalString()。
-	 */
-	protected void appendOriginalInformation() {
-		printString += targetBlock.getInformation().getOriginalString();
-	}
+	@Override
+	public void countWork() {
 
-	/**
-	 * 由子类来添加的其他要打印的信息。
-	 */
-	protected void appendSomeInformation() {
-		//TODO let subClass extend this method
+		//初始化打印字符串，
+		//将printString置为空。
+		printString = "";
+		
+		//子类可以覆盖这个方法来添加这个类需要的子信息。
+		modifyPrintString();
+		
+		//向RLogger打印printString
+		printString();
+		
+		//行数记录加一。
+		++lineCounter;
+		if (hasSubBlock){
+			//打印子链中的信息。
+			printSubBlock();
+		}
 	}
 
 	/**
@@ -89,6 +132,28 @@ public class ScriptPrintSpider extends AbstractBCSpider {
 	 */
 	protected void printString() {
 		RLogger.log(printString);
+	}
+	
+	/**
+	 * 在这个方法中对要打印的信息进行自定义。
+	 */
+	protected void modifyPrintString() {
+		//在首部添加行数。
+		appendLineCounter();
+		
+		//添加层次符号，
+		//几个制表符。
+		appendHierarchy();
+		
+		//添加原始脚本信息。
+		appendOriginalString();
+	}
+	
+	/**
+	 * 打印信息换行。
+	 */
+	protected void newLine() {
+		printString += '\n'; 
 	}
 
 	/**
@@ -107,13 +172,14 @@ public class ScriptPrintSpider extends AbstractBCSpider {
 		}
 	}
 
-	@Override
-	public void reset(){
-		super.reset();
-		lineCounter = 0;
+	/**
+	 * 添加脚本原始信息。
+	 */
+	protected void appendOriginalString() {
+		printString += 
+				targetBlock
+				.getInformation()
+				.getOriginalString();
 	}
 
-	public int getLineCounter() {
-		return lineCounter;
-	}
 }
