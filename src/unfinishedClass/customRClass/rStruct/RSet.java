e9663b2cb79da5c3efff5359be8eb21f737a3f9c
181847 <753090084@qq.com>
2017-07-11 00:47:22 +0800

    增加两种用于存储RClass定义的基本结构RStruct和RSet。
    RStruct用于存储不同类型数据定义，
    
    而RSet用于存储一系列相同类型的RStruct，例如一个RClass的完整定义为RStruct，因为其内部有类型、类名、成员变量……不同类型的定义；而接口父类定义则用一个RSet来存储，因为其内部都是一个类引用类型。

---
+++ src/unfinishedClass/customRClass/rStruct/RSet.java
@@ -0,0 +1,26 @@
+package unfinishedClass.customRClass.rStruct;
+
+import java.util.ArrayList;
+
+/**
+ * 相同类型的RStruc的集合，
+ * 内部可以存储多个同类型的RStruct对象。
+ */
+public class RSet<T extends RStruct> extends ArrayList<T> {
+	/**
+	 * 存储RStruct结构的数组列表。
+	 */
+	protected ArrayList<T> rSetList;
+
+	/**
+	 * 获取指定序号的RStruct对象。
+	 * @param index
+	 * 		对应的序号。
+	 * @return
+	 * 		如果序号超出范围就返回null。
+	 */
+	public T getRStruct(int index) {
+		return rSetList.get(index);
+	}
+
+}
---
+++ src/unfinishedClass/customRClass/rStruct/RStruct.java
@@ -0,0 +1,13 @@
+package unfinishedClass.customRClass.rStruct;
+
+/**
+ * 可以用来存储脚本定义的结构，
+ * 与这种结构相对应的还有一种RSet，
+ * RSet可以存储一系列相同类型的RStruct和RSet（相当于数组），
+ * 而RStruct用于存储不同类型的RStruct和RSet（相当于类的成员变量）。
+ * @param <T>
+ * 		这里的T被限制为RStruct的子类。
+ */
+public interface RStruct {
+
+}
