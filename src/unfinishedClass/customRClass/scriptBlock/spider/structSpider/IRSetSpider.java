111d061e795d86169756643e8e4ceeb845b7f6ba
181847 <753090084@qq.com>
2017-07-11 01:05:43 +0800

    两个关于RStruct的接口定义。
    IRStrucSpider的Spider是 所有 工作成果类型为RStruct的Spider的接口父类，包含一个抽象方法来返回这个工作结果。
    同理对于IRSetSpider就是工作成果类型为RSet类型的Spider的接口父类，
    具体类型由泛参定义。

---
+++ src/unfinishedClass/customRClass/scriptBlock/spider/structSpider/IRSetSpider.java
@@ -0,0 +1,18 @@
+package unfinishedClass.customRClass.scriptBlock.spider.structSpider;
+
+import unfinishedClass.customRClass.rStruct.RSet;
+import unfinishedClass.customRClass.rStruct.RStruct;
+
+/**
+ * 所有工作结果是RSet类型的Spider的接口，
+ * 统一包含一个返回方法。
+ * RSet是一种同类型RStruct的集合。
+ * @author 75309
+ *
+ * @param <R_STRUCT_IN_SET>
+ * 		用于指定集合中RStruct的类型。
+ */
+public interface IRSetSpider<R_STRUCT_IN_SET extends RStruct> {
+	public RSet<R_STRUCT_IN_SET> getRSet();
+
+}
