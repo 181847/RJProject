21fb98d3ec5270df7ab7a9162d521ba11eeb3b8e
181847 <753090084@qq.com>
2017-07-13 15:14:37 +0800

    修改从加载序列生成RClassStruct的代码。
    SequenceStructSpider继承UtilsRStructSpider，使用里面的getRStruct_fromSub_use()来获取子链中的RClassStruct。

--- src/unfinishedClass/customRClass/scriptBlock/spider/structSpider/SequenceStructSpider.java
+++ src/unfinishedClass/customRClass/scriptBlock/spider/structSpider/SequenceStructSpider.java
@@ -2,12 +2,13 @@ package unfinishedClass.customRClass.scriptBlock.spider.structSpider;
 
 import unfinishedClass.customRClass.rStruct.RClassStruct;
 import unfinishedClass.customRClass.scriptBlock.ScriptBlock;
+import unfinishedClass.customRClass.scriptBlock.information.RStructInfo;
 import unfinishedClass.customRClass.scriptBlock.spider.CountableSpider;
 
 /**
  * 将一系列脚本Block的信息提取出来生成RClassStruct。
  */
-public class SequenceStructSpider extends CountableSpider {
+public class SequenceStructSpider extends UtilsRStructSpider {
 
 	public SequenceStructSpider(ScriptBlock targetBlock) {
 		super(targetBlock);
@@ -15,21 +16,14 @@ public class SequenceStructSpider extends CountableSpider {
 
 	@Override
 	public void countWork() {
-		//放置用于都去RClassStruct的结构
-		RClassStructSpider rss = new RClassStructSpider(subBlock);
-		
-		//读取脚本信息。
-		rss.workUntilEnd();
-		
 		//获取脚本结果。
-		RClassStruct rs = rss.getRStruct();
-		//添加原始的脚本来源路径。
-		rs.setSourcePath(targetInfoString);
+		RClassStruct rs = getRStruct_fromSub_use(new RClassStructSpider());
+		//记录原始的脚本来源路径。
+		rs.logSourcePath(targetInfoString);
 		
 		//将读取到的RClass结构放进targetBlock中。
 		targetBlock.setInformation(
-				new StructInfo(
-						rss.getRStruct()));
+				new RStructInfo<RClassStruct>(rs));
 	}
 
 }
