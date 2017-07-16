42ca3fbe778cc8562ba8b7effd81e121af1c3cd0
181847 <753090084@qq.com>
2017-07-16 23:20:00 +0800

    定义包含执行弧线和参数弧线的ArcFieldStruct。

--- src/unfinishedClass/customRClass/rStruct/ArcFieldStruct.java
+++ src/unfinishedClass/customRClass/rStruct/ArcFieldStruct.java
@@ -1,27 +1,81 @@
 package unfinishedClass.customRClass.rStruct;
 
 import unfinishedClass.customRClass.rStruct.baseInterface.IRStruct;
+import unfinishedClass.customRClass.rStruct.baseInterface.IRStruct_contain_arcs;
 
-public class ArcFieldStruct implements IRStruct {
-
-	/**
-	 * 定义执行弧线。
-	 * @param eArcSet
-	 * 		包含多个执行弧线定义的集合。
-	 */
-	public void defineEArcs_by_RSet(RSet<ArcStruct> eArcSet) {
-		// TODO Auto-generated method stub
-		
-	}
+public class ArcFieldStruct implements IRStruct, IRStruct_contain_arcs {
 	
 	/**
-	 * 定义参数弧线。
-	 * @param pArcSet
-	 * 		包含多个参数弧线定义的集合。
+	 * 执行弧线集合。
 	 */
-	public void definePArcs_by_RSet(RSet<ArcStruct> pArcSet) {
-		// TODO Auto-generated method stub
+	protected RSet<ArcStruct> eArcSet;
+	
+	/**
+	 * 参数弧线集合。
+	 */
+	protected RSet<ArcStruct> pArcSet;
+	
+	@Override
+	public void defineArcs_by_RStruct(ArcFieldStruct afStruct) {
+		if (afStruct == null) {
+			throw new IllegalArgumentException(
+					"通过弧线区域定义弧线时，传入参数为null");
+		}
 		
+		defineEArcs_by_RSet(afStruct.getEArcSet());
+		definePArcs_by_RSet(afStruct.getPArcSet());
+	}
+	
+	public void defineEArcs_by_RSet(RSet<ArcStruct> eArcSet) {
+		if (eArcSet == null) {
+			throw new IllegalArgumentException(
+					"通过弧线集合定义执行弧线时，传入参数为null");
+		}
+		
+		for (ArcStruct aStruct : eArcSet) {
+			defineEArc(aStruct);
+		}
+	}
+	
+	public void definePArcs_by_RSet(RSet<ArcStruct> pArcSet) {
+		if (pArcSet == null) {
+			throw new IllegalArgumentException(
+					"通过弧线集合定义参数弧线时，传入参数为null");
+		}
+		
+		for (ArcStruct aStruct : pArcSet) {
+			defineEArc(aStruct);
+		}
+	}
+
+	@Override
+	public void defineEArc(ArcStruct arcStruct) {
+		if (arcStruct == null) {
+			throw new IllegalArgumentException(
+					"不能用null来定义执行弧线。");
+		}
+		
+		eArcSet.add(arcStruct);
+	}
+
+	@Override
+	public void definePArc(ArcStruct arcStruct) {
+		if (arcStruct == null) {
+			throw new IllegalArgumentException(
+					"不能用null来定义参数弧线。");
+		}
+		
+		pArcSet.add(arcStruct);
+	}
+
+	@Override
+	public RSet<ArcStruct> getEArcSet() {
+		return eArcSet;
+	}
+
+	@Override
+	public RSet<ArcStruct> getPArcSet() {
+		return pArcSet;
 	}
 
 }
